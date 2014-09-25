b053ed6f66f4237d49f02e579b0c89b524832aea
Tony Miller <mcfiredrill@gmail.com>
2014-09-25 14:05:38 +0900

    radios controller

--- app/controllers/radios_controller.rb
+++ app/controllers/radios_controller.rb
@@ -1,8 +1,10 @@
 class RadiosController < ApplicationController
   load_and_authorize_resource
   def index
+    @radio = Radio.new
   end
   def create
+    @radio.user_id = current_user.id
     if @radio.save
       redirect_to 'index'
     else
