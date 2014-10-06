e42a3f750ca286bed30cf6d7c04c289718075b27
Tony Miller <mcfiredrill@gmail.com>
2014-10-07 01:23:24 +0900

    fix setting env var in container

--- app/workers/radio_booter.rb
+++ app/workers/radio_booter.rb
@@ -3,8 +3,8 @@ class RadioBooter
 
   def perform radio_id
     radio = Radio.find radio_id
-    container = Docker::Container.create( 'Image' => 'mcfiredrill/radio')
-    container.start("PublishAllPorts" => "true", "Env"=> {"VIRTUAL_HOST"=>radio.virtual_host})
+    container = Docker::Container.create( 'Image' => 'mcfiredrill/radio', "Env"=> ["VIRTUAL_HOST=#{radio.virtual_host}"])
+    container.start("PublishAllPorts" => "true")
     radio.update docker_container_id: container.id
   end
 end
