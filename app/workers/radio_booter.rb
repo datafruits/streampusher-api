02838f554a9f605aff7b6e51d3a126b3556bf289
Tony Miller <mcfiredrill@gmail.com>
2014-10-11 00:22:12 +0900

    setting the domainname in the docker instead of env variable

--- app/workers/radio_booter.rb
+++ app/workers/radio_booter.rb
@@ -3,7 +3,7 @@ class RadioBooter
 
   def perform radio_id
     radio = Radio.find radio_id
-    container = Docker::Container.create( 'Image' => 'mcfiredrill/radio', "Env"=> ["VIRTUAL_HOST=#{radio.virtual_host}"])
+    container = Docker::Container.create('Image' => 'mcfiredrill/radio', "Domainname"=> radio.virtual_host)
     container.start("PublishAllPorts" => "true")
     radio.update docker_container_id: container.id
   end
