45be6e9e9130e3d0e44a455b31f76617b8cddc14
Tony Miller <mcfiredrill@gmail.com>
2014-10-16 21:04:37 +0900

    stop tracking database.yml, add navbar to all layouts

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141001155219) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "plans", force: true do |t|
     t.decimal  "price"
     t.datetime "created_at"
@@ -56,7 +59,7 @@ ActiveRecord::Schema.define(version: 20141001155219) do
     t.string   "role"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
