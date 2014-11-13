b063825100702985f98ba5d0b33678d01233448a
Tony Miller <mcfiredrill@gmail.com>
2014-11-12 17:53:32 -0800

    set env RADIO_NAME in container

--- app/workers/radio_booter.rb
+++ app/workers/radio_booter.rb
@@ -4,7 +4,7 @@ class RadioBooter
   def perform radio_id
     radio = Radio.find radio_id
     container = Docker::Container.create('Image' => 'mcfiredrill/radio', "Domainname"=> radio.virtual_host)
-    container.start("PublishAllPorts" => "true")
+    container.start("PublishAllPorts" => "true", "Env"=> {"RADIO_NAME"=>radio.name})
     radio.update docker_container_id: container.id
   end
 end
