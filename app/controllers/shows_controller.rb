3684b66db29a52f367becf023c3590d15df70599
Tony Miller <mcfiredrill@gmail.com>
2014-12-03 18:59:58 +0900

    working on shows

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -4,13 +4,25 @@ class ShowsController < ApplicationController
     @current_radio = current_radio
     @shows = @current_radio.shows
     @show = @shows.new
+
+    respond_to do |format|
+      format.html
+      format.json { @shows }
+    end
   end
 
   def create
+    if @show.save
+      redirect_to shows_path
+    else
+      @current_radio = current_radio
+      @shows = @current_radio.shows
+      render 'index'
+    end
   end
 
   private
   def create_params
-
+    params.require(:show).permit(:dj_id, :title, :start_at, :end_at, :radio_id)
   end
 end
