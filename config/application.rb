5cc66878e8ad65f33d294c0364775f86da8269b3
Tony Miller <mcfiredrill@gmail.com>
2014-12-31 23:23:16 +0900

    load application.yml config

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,12 @@ module StreamPusher
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    #
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'application.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
