d455c23aedb6f28ce11c15489850e3fe4fa6786f
Tony Miller <mcfiredrill@gmail.com>
2015-01-01 12:09:38 +0900

    apparentally its 'name' not 'Name' in the docker api :(

--- app/workers/radio_booter_worker.rb
+++ app/workers/radio_booter_worker.rb
@@ -1,28 +1,30 @@
+require 'uri'
+
 class RadioBooterWorker
   include Sidekiq::Worker
 
   def perform radio_id
     radio = Radio.find radio_id
     radio_name = radio.name
-    redis = Redis.new
+    redis = Redis.new host: URI.parse(ENV['DOCKER_HOST']).hostname
 
     if !radio.icecast_container.blank?
       icecast_container = radio.icecast_container
     else
-      icecast_container = Docker::Container.create('Image' => 'mcfiredrill/icecast')
+      icecast_container = Docker::Container.create('Image' => 'mcfiredrill/icecast', 'name' => "#{radio_name}/icecast")
       radio.update icecast_container_id: icecast_container.id
     end
-    icecast_container.start("PublishAllPorts" => "true", 'Name' => "#{radio_name}/icecast")
+    icecast_container.start("PublishAllPorts" => "true", )
     redis.hset 'proxy-domain', radio.virtual_host, host_port(icecast_container)
 
     if !radio.liquidsoap_container.blank?
       liquidsoap_container = radio.liquidsoap_container
     else
-      liquidsoap_container = Docker::Container.create('Image' => 'mcfiredrill/liquidsoap',
+      liquidsoap_container = Docker::Container.create('Image' => 'mcfiredrill/liquidsoap', 'name' => "#{radio_name}/liquidsoap",
                                                       "Domainname"=> radio.virtual_host, 'Links' => ["#{radio_name}/icecast:icecast","redis:redis"])
       radio.update liquidsoap_container_id: liquidsoap_container.id
     end
-    liquidsoap_container.start("PublishAllPorts" => "true", "Env"=> {"RADIO_NAME"=>radio_name}, 'Name' => "#{radio_name}/liquidsoap")
+    liquidsoap_container.start("PublishAllPorts" => "true", "Env"=> {"RADIO_NAME"=>radio_name})
     # persist this in redis too? watdo? :\
   end
 
