644ba0647e5363d0c3b16e1c4c4405333c12875c
Tony Miller <mcfiredrill@gmail.com>
2015-01-17 15:48:35 +0900

    fix reboot radio flash

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -16,6 +16,7 @@ class AdminController < ApplicationController
     authorize! :admin, :restart_radio
     @radio = Radio.find params[:id]
     @radio.boot_radio
-    redirect_to action: :radios, notice: 'radio rebooting...'
+    flash[:notice] = 'radio rebooting...'
+    redirect_to action: :radios
   end
 end
