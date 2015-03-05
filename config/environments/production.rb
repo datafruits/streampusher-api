1cf80e674fa690ab0a19ae4be9def8bee77d8b58
Tony Miller <mcfiredrill@gmail.com>
2015-03-05 13:19:46 +0900

    configure mailer for mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,19 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => "streampusher.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.mandrillapp.com',
+    :port           => 25,
+    :authentication => :plain,
+    :user_name      => ENV['MANDRILL_USERNAME'],
+    :password       => ENV['MANDRILL_PASSWORD'],
+    :domain         => 'streampusher.com',
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -79,4 +92,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.use ExceptionNotification::Rack,
+    email: {
+    email_prefix: "[Exception] ",
+    sender_address: %{"Exception Notifier" <exception@streampusher.com>},
+    exception_recipients: %w{info@datafruits.fm}
+  }
 end
