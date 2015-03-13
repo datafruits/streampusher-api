2af8bcbb18349d86ba6722636045a64fa7cd76ac
Tony Miller <mcfiredrill@gmail.com>
2015-03-13 15:18:29 +0900

    fix radio_booter_worker

--- app/workers/radio_booter_worker.rb
+++ app/workers/radio_booter_worker.rb
@@ -17,13 +17,13 @@ class RadioBooterWorker
     icecast_container = DockerWrapper.find_or_create 'mcfiredrill/icecast:latest', "#{radio_name}_icecast"
     radio.update icecast_container_id: icecast_container.id
     if ::Rails.env.production?
-      UFW.close_port icecast_container.host_port
+      UFW.close_port icecast_container.host_port(8000)
     end
     icecast_container.stop
     icecast_container.start
     redis.hset 'proxy-domain', "#{radio.virtual_host}/icecast", icecast_container.host_port(8000)
     if ::Rails.env.production?
-      UFW.open_port icecast_container.host_port
+      UFW.open_port icecast_container.host_port(8000)
     end
 
     liquidsoap_container = DockerWrapper.find_or_create 'mcfiredrill/liquidsoap:latest',
@@ -32,13 +32,13 @@ class RadioBooterWorker
       ["#{radio_name}_icecast:icecast","streampusher_redis_1:redis"]
     radio.update liquidsoap_container_id: liquidsoap_container.id
     if ::Rails.env.production?
-      UFW.close_port icecast_container.host_port
+      UFW.close_port liquidsoap_container.host_port(9000)
     end
     liquidsoap_container.stop
     liquidsoap_container.start
     redis.hset 'proxy-domain', "#{radio.virtual_host}/liquidsoap", liquidsoap_container.host_port(9000)
     if ::Rails.env.production?
-      UFW.open_port icecast_container.host_port
+      UFW.open_port liquidsoap_container.host_port(9000)
     end
     radio.playlists.each do |playlist|
       SavePlaylistToRedisWorker.perform_async playlist.id
