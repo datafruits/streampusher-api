0828f463fb815562ee60b173eee04deb902f8d73
Tony Miller <mcfiredrill@gmail.com>
2015-03-17 00:46:56 +0900

    probably a bad idea :(

--- lib/ufw.rb
+++ lib/ufw.rb
@@ -2,12 +2,12 @@ require 'cocaine'
 
 class UFW
   def self.open_port port
-    line = Cocaine::CommandLine.new("ufw", "allow :port")
-    line.run(port: port)
+    line = Cocaine::CommandLine.new("sudo ufw", "allow #{port}")
+    line.run
   end
 
   def self.close_port port
-    line = Cocaine::CommandLine.new("ufw", "deny :port")
-    line.run(port: port)
+    line = Cocaine::CommandLine.new("sudo ufw", "deny #{port}")
+    line.run
   end
 end
