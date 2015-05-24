5caba770fee7b50726804f3a205b3877bcdefaf5
Tony Miller <mcfiredrill@gmail.com>
2015-05-24 21:12:55 +0900

    edit/update podcast

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -16,6 +16,22 @@ class PodcastsController < ApplicationController
     end
   end
 
+  def edit
+    @current_radio = current_radio
+    @podcast = @current_radio.podcasts.find params[:id]
+  end
+
+  def update
+    @podcast.attributes = podcast_params
+    if @podcast.save
+      redirect_to podcasts_path
+    else
+      @current_radio = current_radio
+      @podcasts = @current_radio.podcasts
+      render 'index'
+    end
+  end
+
   def show
     @podcast = current_radio.podcasts.find_by_name(params[:id])
     respond_to do |format|
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :recordings
-  resources :podcasts, only: [:index, :show, :create]
+  resources :podcasts
 
   resources :shows
   resources :scheduled_shows
