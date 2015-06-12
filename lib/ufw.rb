429d9c8f4201f4b5d8381aad0ed80027782da007
Tony Miller <mcfiredrill@gmail.com>
2015-06-12 15:01:09 +0900

    use full path in ufw

--- lib/ufw.rb
+++ lib/ufw.rb
@@ -2,12 +2,12 @@ require 'cocaine'
 
 class UFW
   def self.open_port port
-    line = Cocaine::CommandLine.new("sudo ufw", "allow #{port}")
+    line = Cocaine::CommandLine.new("sudo /usr/sbin/ufw", "allow #{port}")
     line.run
   end
 
   def self.close_port port
-    line = Cocaine::CommandLine.new("sudo ufw", "deny #{port}")
+    line = Cocaine::CommandLine.new("sudo /usr/sbin/ufw", "deny #{port}")
     line.run
   end
 end
