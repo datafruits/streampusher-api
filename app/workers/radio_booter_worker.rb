548ce76adfca83442b6a4c509397f6cf9f21f6d0
Tony Miller <mcfiredrill@gmail.com>
2015-06-13 21:24:59 +0900

    call UFW again

--- app/workers/radio_booter_worker.rb
+++ app/workers/radio_booter_worker.rb
@@ -16,21 +16,21 @@ class RadioBooterWorker < ActiveJob::Base
 
     icecast_container = DockerWrapper.find_or_create 'mcfiredrill/icecast:latest', "#{radio_name}_icecast"
     radio.update icecast_container_id: icecast_container.id
-    #if ::Rails.env.production?
-    #  port = icecast_container.host_port(8000)
-    #  if port
-    #    UFW.close_port port
-    #  end
-    #end
+    if ::Rails.env.production?
+     port = icecast_container.host_port(8000)
+     if port
+       UFW.close_port port
+     end
+    end
     icecast_container.stop
     icecast_container.start
     redis.hset 'proxy-domain', "#{radio.virtual_host}/icecast", icecast_container.host_port(8000)
-    # if ::Rails.env.production?
-    #   port = icecast_container.host_port(8000)
-    #   if port
-    #     UFW.open_port port
-    #   end
-    # end
+    if ::Rails.env.production?
+      port = icecast_container.host_port(8000)
+      if port
+        UFW.open_port port
+      end
+    end
 
     liquidsoap_container = DockerWrapper.find_or_create 'mcfiredrill/liquidsoap:latest',
       "#{radio_name}_liquidsoap",
@@ -38,21 +38,21 @@ class RadioBooterWorker < ActiveJob::Base
       ["#{radio_name}_icecast:icecast","streampusher_redis_1:redis"],
       ["#{radio.tracks_directory}:/home/liquidsoap/tracks"]
     radio.update liquidsoap_container_id: liquidsoap_container.id
-    #if ::Rails.env.production?
-    #  port = liquidsoap_container.host_port(9000)
-    #  if port
-    #    UFW.close_port port
-    #  end
-    #end
+    if ::Rails.env.production?
+     port = liquidsoap_container.host_port(9000)
+     if port
+       UFW.close_port port
+     end
+    end
     liquidsoap_container.stop
     liquidsoap_container.start
     redis.hset 'proxy-domain', "#{radio.virtual_host}/liquidsoap", liquidsoap_container.host_port(9000)
-    #if ::Rails.env.production?
-    #  port = liquidsoap_container.host_port(9000)
-    #  if port
-    #    UFW.open_port port
-    #  end
-    #end
+    if ::Rails.env.production?
+     port = liquidsoap_container.host_port(9000)
+     if port
+       UFW.open_port port
+     end
+    end
     radio.playlists.each do |playlist|
       SavePlaylistToRedisWorker.perform_later playlist.id
     end
