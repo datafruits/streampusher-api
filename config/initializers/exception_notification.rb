927bcb13899f5c92ca1097a9f533f82cb7f7541c
Tony Miller <mcfiredrill@gmail.com>
2015-06-25 20:49:37 +0900

    don't use exception_notification on production

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -2,6 +2,11 @@ require 'exception_notification/rails'
 require 'exception_notification/sidekiq'
 
 ExceptionNotification.configure do |config|
+  # Adds a condition to decide when an exception must be ignored or not.
+  # The ignore_if method can be invoked multiple times to add extra conditions.
+  config.ignore_if do |exception, options|
+    not Rails.env.production?
+  end
   config.add_notifier :email, {
     :email_prefix => "[Exception] ",
     :sender_address => %{"Exception Notifier" <exception@streampusher.com>},
