4e5fb33b017b1e83e969ee53bd17b2ba6f8533c6
Tony Miller <mcfiredrill@gmail.com>
2015-07-06 12:29:36 +0900

    don't need to assign @current_radio anymore

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,7 +1,6 @@
 class PodcastsController < ApplicationController
   load_and_authorize_resource except: [:show]
   def index
-    @current_radio = current_radio
     @podcasts = @current_radio.podcasts
     @podcast = Podcast.new
   end
@@ -10,14 +9,12 @@ class PodcastsController < ApplicationController
     if @podcast.save
       redirect_to podcasts_path
     else
-      @current_radio = current_radio
       @podcasts = @current_radio.podcasts
       render 'index'
     end
   end
 
   def edit
-    @current_radio = current_radio
     @podcast = @current_radio.podcasts.find params[:id]
   end
 
@@ -26,18 +23,18 @@ class PodcastsController < ApplicationController
     if @podcast.save
       redirect_to podcasts_path
     else
-      @current_radio = current_radio
       @podcasts = @current_radio.podcasts
       render 'index'
     end
   end
 
   def show
-    @podcast = current_radio.podcasts.find_by_name(params[:id])
+    @podcast = @current_radio.podcasts.find_by_name(params[:id])
     respond_to do |format|
       format.xml { render 'show', layout: false }
     end
   end
+
   private
   def podcast_params
     params.require(:podcast).permit(:name, :playlist_id, :radio_id)
