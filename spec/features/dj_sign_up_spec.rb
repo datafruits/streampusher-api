c695a4ded11f7e7cdd8f54b7f8df396498a393cd
Tony Miller <mcfiredrill@gmail.com>
2015-07-09 23:36:00 +0900

    renamed dj_sign_up_spec

--- spec/features/dj_sign_up_spec.rb
+++ spec/features/dj_sign_up_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 feature 'dj sign up' do
-  scenario 'admin adds new djs to radio'
+  scenario 'owner adds new djs to radio'
   scenario 'dj can sign up if admin enables dj self signup'
 end
