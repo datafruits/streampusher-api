5d9146e12d78d0e97b228be62519b2a93e9c9dff
Tony Miller <mcfiredrill@gmail.com>
2015-07-18 13:37:00 +0900

    add tweet scheduled shows text

---
+++ app/helpers/scheduled_shows_helper.rb
@@ -0,0 +1,20 @@
+module ScheduledShowsHelper
+  def multiple_timezones(time)
+    timezones = {"PST" => "Pacific Time (US & Canada)",
+                 "EST" => "Eastern Time (US & Canada)",
+                 "UK"  => "London",
+                 "EU"  => "Stockholm",
+                 "日本"=> "Tokyo"}
+    time_string = ""
+
+    timezones.each do |k,v|
+      time_string << "[#{Time.zone.parse(time.to_s).in_time_zone(v).strftime("%H:%M")} #{k}] "
+    end
+
+    time_string
+  end
+
+  def tweet_text(show)
+    "#{show.dj.username} - #{show.title} on @datafruits #{show.start_at.strftime("%m/%d")} - #{multiple_timezones(show.start_at)}"
+  end
+end
--- app/serializers/scheduled_show_serializer.rb
+++ app/serializers/scheduled_show_serializer.rb
@@ -1,4 +1,8 @@
 class ScheduledShowSerializer < ActiveModel::Serializer
-  attributes :id, :start, :end, :title, :image_url, :dj
+  include ScheduledShowsHelper
+  attributes :id, :start, :end, :title, :image_url, :dj, :tweet_content
 
+  def tweet_content
+    tweet_text(object)
+  end
 end
