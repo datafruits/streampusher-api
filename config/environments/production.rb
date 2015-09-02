db729288c3541c933da8f60fb65b998dc0b4bf93
Tony Miller <mcfiredrill@gmail.com>
2015-09-03 06:44:11 +0900

    try authentication :plain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.mandrillapp.com',
-    :port           => 587,
-    :authentication => :login,
+    :port           => '587',
+    :authentication => :plain,
     :user_name      => ENV['MANDRILL_USERNAME'],
     :password       => ENV['MANDRILL_PASSWORD'],
     :domain         => 'streampusher.com',
