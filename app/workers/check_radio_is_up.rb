55d77d5ec4387601ac0e6949d3a4bccec09ab135
Tony Miller <mcfiredrill@gmail.com>
2015-09-02 14:03:30 +0900

    use my dongle checker

--- app/workers/check_radio_is_up.rb
+++ app/workers/check_radio_is_up.rb
@@ -1,16 +1,16 @@
-require 'net/http'
-require 'uri'
-
 class CheckRadioIsUp < ActiveJob::Base
   queue_as :default
 
   def perform
     Radio.find_each do |radio|
-      res = Net::HTTP.get_response(URI(radio.icecast_panel_url))
-      if res.code.to_i != 200
+      url = radio.icecast_panel_url
+      res = Net::HTTP.get_response(URI("http://is-this-dongle-working.herokuapp.com/?site=#{url.to_s}"))
+      if res.body == "yes"
+        puts "radio is up"
+      elsif res.body == "no"
         AdminMailer.radio_not_reachable radio
       else
-        puts "radio is up"
+        puts "error: #{res.body}"
       end
     end
   end
