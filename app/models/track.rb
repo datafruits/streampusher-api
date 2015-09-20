99420dc28bf6b42ea49175a5b11ba87e1b9ffcad
Tony Miller <mcfiredrill@gmail.com>
2015-09-20 11:48:47 +0900

    new so_id3 stuff

--- app/models/track.rb
+++ app/models/track.rb
@@ -7,7 +7,7 @@ class Track < ActiveRecord::Base
            s3_credentials: { bucket: "streampusher",
                              access_key_id: ENV['S3_KEY'],
                              secret_access_key: ENV['S3_SECRET'] }
-  after_save :download
+  after_tags_synced :download # will this happen before the update tags job finishes? :(
 
   def file_basename
     File.basename self.audio_file_name.to_s
@@ -19,6 +19,16 @@ class Track < ActiveRecord::Base
 
   def local_directory
     self.radio.tracks_directory
+  end
+
+  def display_name
+    if self.artist.present?
+      "#{self.artist} - #{self.title}"
+    elsif self.title.present?
+      "#{self.title}"
+    else
+      self.file_basename
+    end
   end
 
   def download
