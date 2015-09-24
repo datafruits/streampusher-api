c4f5760308a7c4030c971551ca26aeb801991e9b
Tony Miller <mcfiredrill@gmail.com>
2015-09-24 20:00:22 +0900

    add title to playlist_track_serializer

--- app/serializers/playlist_track_serializer.rb
+++ app/serializers/playlist_track_serializer.rb
@@ -1,7 +1,11 @@
 class PlaylistTrackSerializer < ActiveModel::Serializer
-  attributes :audio_file_name, :podcast_published_date, :id, :track_id, :playlist_id
+  attributes :audio_file_name, :podcast_published_date, :id, :track_id, :playlist_id, :title
 
   def audio_file_name
     object.track.audio_file_name
+  end
+
+  def title
+    object.track.display_name
   end
 end
