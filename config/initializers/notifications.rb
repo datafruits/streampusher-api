17b0d712ecdd6ee8ca8f9bb1a8916a45791d7ea7
Tony Miller <mcfiredrill@gmail.com>
2015-10-12 22:13:11 +0900

    only run ActiveSupport::Notifications code in production

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -1,16 +1,18 @@
-events = [
-          "user.signup",
-          "playlist.created",
-          "track.created",
-          "dj.created",
-          "podcast.created",
-          "scheduled_show.created",
-          "show.created"
-         ]
+if Rails.env.production?
+  events = [
+            "user.signup",
+            "playlist.created",
+            "track.created",
+            "dj.created",
+            "podcast.created",
+            "scheduled_show.created",
+            "show.created"
+           ]
 
-events.each do |event|
-  ActiveSupport::Notifications.subscribe event do |*args|
-    event = ActiveSupport::Notifications::Event.new *args
-    SlackNotifier.perform_later "#{event.name}: #{event.payload.inspect}"
+  events.each do |event|
+    ActiveSupport::Notifications.subscribe event do |*args|
+      event = ActiveSupport::Notifications::Event.new *args
+      SlackNotifier.perform_later "#{event.name}: #{event.payload.inspect}"
+    end
   end
 end
