fdf216b091193b0c08702c81600c5cb5b3afa76b
Tony Miller <mcfiredrill@gmail.com>
2015-10-12 21:02:18 +0900

    order recordings by created_at

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -4,7 +4,8 @@ class Recording < ActiveRecord::Base
   belongs_to :show
   has_attached_file :file
 
+  default_scope { order(created_at: :desc) }
+
   validates_attachment :file, presence: true,
     content_type: { content_type: [ "audio/mpeg", "audio/mp3"] }
-
 end
