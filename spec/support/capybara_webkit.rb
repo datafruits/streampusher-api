d2778e907c83882b8cf4d2b80bc8e060473b4626
Tony Miller <mcfiredrill@gmail.com>
2015-11-26 10:52:19 +0900

    allow urls

--- spec/support/capybara_webkit.rb
+++ spec/support/capybara_webkit.rb
@@ -1,4 +1,5 @@
-# Capybara::Webkit.configure do |config|
-#   config.allow_url "js.stripe.com"
-#   config.allow_url "s3.amazonaws.com"
-# end
+Capybara::Webkit.configure do |config|
+  config.allow_url "js.stripe.com"
+  config.allow_url "s3.amazonaws.com"
+  config.allow_url "datafruits.streampusher.com"
+end
