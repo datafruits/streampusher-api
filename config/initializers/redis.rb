8d79fffac803d323a50a5dded341a1a49ea58a0a
Tony Miller <mcfiredrill@gmail.com>
2015-12-06 13:38:21 +0900

    clean up redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,10 +1,1 @@
-if ::Rails.env.production?
-  Redis.current = Redis.new
-else
-  #if ENV['DOCKER_HOST']
-  #  Redis.current = Redis.new host: URI.parse(ENV['DOCKER_HOST']).hostname
-  #else
-    # for CI
-    Redis.current = Redis.new
-  #end
-end
+Redis.current = Redis.new
