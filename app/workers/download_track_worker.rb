5d050dc4fc1cbf382e726c8e535f60040c51548e
Tony Miller <mcfiredrill@gmail.com>
2015-12-11 11:52:19 +0900

    use ruby to download tracks

--- app/workers/download_track_worker.rb
+++ app/workers/download_track_worker.rb
@@ -1,9 +1,15 @@
+require 'open-uri'
+
 class DownloadTrackWorker < ActiveJob::Base
   queue_as :default
 
   def perform track_id
-    track = Track.find track_id
-    line = Cocaine::CommandLine.new("wget", "-q :file -O :dest")
-    line.run(file: track.audio_file_name, dest: track.local_path)
+    track = Track.find_by! id: track_id
+
+    File.open(track.local_path, "wb") do |saved_file|
+      open(track.audio_file_name, "rb") do |read_file|
+        saved_file.write(read_file.read)
+      end
+    end
   end
 end
