e74aef9c380b3b7b42fbbf4fe8efbc99701c0643
Tony Miller <mcfiredrill@gmail.com>
2016-01-24 20:15:11 -0500

    remove redis persistance from scheduling
    Conflicts:
    	spec/models/scheduled_show_spec.rb
    
    Former-commit-id: 38cc5391608f9b4f93d3e30608f9e836a8768c32

--- spec/models/scheduled_show_spec.rb
+++ spec/models/scheduled_show_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 require 'sidekiq/testing'
-require 'mock_redis'
 
 RSpec.describe ScheduledShow, :type => :model do
   before do
     Sidekiq::Testing.fake!
-    Redis.current = MockRedis.new
 
     @radio = Radio.create name: 'datafruits', subscription_id: 1
     @dj = User.create role: 'dj', username: 'dakota', email: "dakota@gmail.com", password: "2boobies", time_zone: "UTC"
@@ -33,41 +31,6 @@ RSpec.describe ScheduledShow, :type => :model do
       end_at = Chronic.parse("today at 2:15 pm").utc
       @scheduled_show = ScheduledShow.create radio: @radio, show: @show, start_at: start_at, end_at: end_at
       expect(@scheduled_show.errors[:end_at]).to be_present
-    end
-  end
-  describe "redis persistence" do
-    before do
-      Time.zone = 'UTC'
-      Timecop.freeze Time.local(1990)
-      @scheduled_show = ScheduledShow.create radio: @radio, show: @show, start_at: @start_at, end_at: @end_at
-    end
-
-    after do
-      Timecop.return
-    end
-
-    it 'gets time keys' do
-      expect(@scheduled_show.time_keys).to eq ["#{@date}:04h15m", "#{@date}:04h30m", "#{@date}:04h45m", "#{@date}:05h00m",
-                                              "#{@date}:05h15m", "#{@date}:05h30m", "#{@date}:05h45m", "#{@date}:06h00m"]
-    end
-
-    it "gets redis keys" do
-      expect(@scheduled_show.redis_keys).to eq ["#{@radio.name}:schedule:#{@date}:04h15m",
-                                                "#{@radio.name}:schedule:#{@date}:04h30m",
-                                                "#{@radio.name}:schedule:#{@date}:04h45m",
-                                                "#{@radio.name}:schedule:#{@date}:05h00m",
-                                                "#{@radio.name}:schedule:#{@date}:05h15m",
-                                                "#{@radio.name}:schedule:#{@date}:05h30m",
-                                                "#{@radio.name}:schedule:#{@date}:05h45m",
-                                                "#{@radio.name}:schedule:#{@date}:06h00m"]
-    end
-
-    it "persists to redis" do
-      redis = double
-      @scheduled_show.redis_keys.each do |key|
-        expect(redis).to receive(:set).with(key, @scheduled_show.show.playlist.redis_key)
-      end
-      @scheduled_show.persist_to_redis redis
     end
   end
 
