6f0c2818ce3af9ce6775a56bdd7ff554f5c9b32f
Tony Miller <mcfiredrill@gmail.com>
2016-02-28 13:53:23 +0900

    try to upgrade ember-data
    Former-commit-id: e53b646d69a17bf109d13faa31bc83f37b1c9659

--- frontend/app/models/playlist.js
+++ frontend/app/models/playlist.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr(),
-  playlistTracks: DS.hasMany('playlistTrack')
+  playlistTracks: DS.hasMany('playlist-track')
 });
