6bab1fb49b275abfd5efe82cf2c48c2c924a617d
Tony Miller <mcfiredrill@gmail.com>
2016-03-02 13:09:31 +0900

    add html email for welcome_email
    Former-commit-id: 249717f5e5a073d2b3eb004573e81f7ef06602a7

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,15 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  #
+  #
+  # mailcatcher
+  # config.action_mailer.default_url_options = { :host => "streampusher.com" }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.smtp_settings = {
+  #   :address        => 'localhost',
+  #   :port           => '1025'
+  # }
 end
