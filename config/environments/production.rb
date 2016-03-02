2f5320c91bfbaa067e01f12b4a6858fd542c34a1
Tony Miller <mcfiredrill@gmail.com>
2016-03-02 12:10:22 +0900

    switch to sendgrid for email
    Former-commit-id: a887e064003284143f8dd0d2da5ffa949854728e

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,11 +69,11 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    :address        => 'smtp.mandrillapp.com',
+    :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['MANDRILL_USERNAME'],
-    :password       => ENV['MANDRILL_PASSWORD'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'streampusher.com',
     :enable_starttls_auto => true
   }
