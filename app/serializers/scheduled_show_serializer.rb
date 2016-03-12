4e982c048333e7bbd4954234c5a57161159281c6
Tony Miller <mcfiredrill@gmail.com>
2016-03-12 21:05:27 +0900

    conditionally render image_url and thumb_image_url in scheduled_show_serializer
    Former-commit-id: 145095e05f1ab110f7698c5b374d86a4037af5f2

--- app/serializers/scheduled_show_serializer.rb
+++ app/serializers/scheduled_show_serializer.rb
@@ -2,6 +2,18 @@ class ScheduledShowSerializer < ActiveModel::Serializer
   include ScheduledShowsHelper
   attributes :id, :start, :end, :title, :image_url, :thumb_image_url, :dj, :tweet_content, :description
 
+  def image_url
+    if object.image.present?
+      object.image_url
+    end
+  end
+
+  def thumb_image_url
+    if object.image.present?
+      object.thumb_image_url
+    end
+  end
+
   def tweet_content
     tweet_text(object)
   end
