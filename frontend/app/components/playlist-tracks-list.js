826b61735341e517f913991471a45853ed7c891c
Tony Miller <mcfiredrill@gmail.com>
2016-04-02 17:53:57 +0900

    implement new playlist button
    Former-commit-id: d83c009ac0f9f0464c0dcf496fa41104405bae35

--- frontend/app/components/playlist-tracks-list.js
+++ frontend/app/components/playlist-tracks-list.js
@@ -2,26 +2,47 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
+  isEditingSettings: false,
   isEditing: false,
   actions: {
     editPlaylist: function(){
       this.toggleProperty('isEditing');
       //$("#edit-playlist-modal").modal("toggle");
     },
+    editPlaylistSettings: function(){
+      this.toggleProperty('isEditingSettings');
+      //$("#edit-playlist-modal").modal("toggle");
+    },
+    newPlaylist: function(){
+      var store = this.get('store');
+      var playlist = store.createRecord('playlist');
+      this.set('playlist', playlist);
+      this.set('isEditing', true);
+    },
     selectInterpolatedPlaylistId: function(playlistId) {
       this.set('interpolatedPlaylistId', playlistId);
     },
-    save: function() {
+    saveSettings: function() {
       var playlist = this.get('playlist');
       playlist.set('interpolatedPlaylistId', this.get('interpolatedPlaylistId'));
+      var onSuccess = () =>{
+        this.set('isEditingSettings', false);
+      };
+      var onFail = () =>{
+        console.log("playlist settings save failed");
+      };
+      playlist.save().then(onSuccess, onFail);
+      //$("#edit-playlist-modal").modal("toggle");
+    },
+    save: function() {
+      var playlist = this.get('playlist');
       var onSuccess = () =>{
         this.set('isEditing', false);
       };
       var onFail = () =>{
-        console.log("track save failed");
+        console.log("playlist save failed");
       };
       playlist.save().then(onSuccess, onFail);
-      //$("#edit-playlist-modal").modal("toggle");
     }
   }
 });
