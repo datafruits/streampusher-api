813d250047e30aba740960767c48a73bfe1f83ce
Tony Miller <mcfiredrill@gmail.com>
2016-05-20 14:18:00 +0900

    add validation for playlist name presence
    Former-commit-id: 2360a4f8906ca16b1c1b198f1bea06a8f2591eae

--- frontend/app/models/playlist.js
+++ frontend/app/models/playlist.js
@@ -1,6 +1,12 @@
 import DS from 'ember-data';
+import { validator, buildValidations } from 'ember-cp-validations';
 
-export default DS.Model.extend({
+const Validations = buildValidations({
+  name: validator('presence', { presence: true, message: "cannot be blank", debounce: 500}),
+});
+
+
+export default DS.Model.extend(Validations, {
   name: DS.attr(),
   playlistTracks: DS.hasMany('playlist-track'),
   interpolatedPlaylistTrackIntervalCount: DS.attr(),
