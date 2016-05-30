2b3ce01f6116584079d984fcd058e983e963db2f
Tony Miller <mcfiredrill@gmail.com>
2016-05-30 22:41:11 +0900

    Revert "use localhost for now"
    This reverts commit c5c44a204a1f8fba81f1d0b9007f6fab417e1474 [formerly 83d53cf4ce8c2fba6720f0f4e18028556394f582].
    
    
    Former-commit-id: 12c2f35336642a2506b28ef9837f0fe9580c7579

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-host = ENV['REDIS_PORT_6379_TCP_ADDR'] || 'localhost'
+host = ENV['REDIS_PORT_6379_TCP_ADDR'] || 'redis'
 port = ENV['REDIS_PORT_6379_TCP_PORT'] || 6379
 Redis.current = Redis.new host: host, port: port
