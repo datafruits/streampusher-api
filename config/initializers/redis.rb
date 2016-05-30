70db7ee279963d18a450c6b3245b37f334c6be73
Tony Miller <mcfiredrill@gmail.com>
2016-05-30 22:28:14 +0900

    use localhost for now
    Former-commit-id: 56826d875b21d9308602df18b12bb05309dac64c

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-host = ENV['REDIS_PORT_6379_TCP_ADDR'] || 'redis'
+host = ENV['REDIS_PORT_6379_TCP_ADDR'] || 'localhost'
 port = ENV['REDIS_PORT_6379_TCP_PORT'] || 6379
 Redis.current = Redis.new host: host, port: port
