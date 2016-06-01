7b08cad2e82c3c5a0bb417952c96bfb2569b5763
Tony Miller <mcfiredrill@gmail.com>
2016-06-01 13:44:14 +0900

    more terrible
    Former-commit-id: a0fe9840cca94db651e17c354691d1bfd68b7fea

--- app/serializers/podcast_serializer.rb
+++ app/serializers/podcast_serializer.rb
@@ -2,7 +2,10 @@ class PodcastSerializer < ActiveModel::Serializer
   attributes :id, :name, :tracks
 
   def tracks
-    object.playlist.playlist_tracks.unscoped.order("podcast_published_date DESC").map do |playlist_track|
+    object.playlist.playlist_tracks
+      .unscoped
+      .where(playlist_id: object.playlist.id)
+      .order("podcast_published_date DESC").map do |playlist_track|
       PlaylistTrackSerializer.new(playlist_track, root: false)
     end
   end
