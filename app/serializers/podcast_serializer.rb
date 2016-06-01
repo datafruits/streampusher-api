a6995501cfac9f1921626226be4f16401b87b787
Tony Miller <mcfiredrill@gmail.com>
2016-06-01 13:10:43 +0900

    fix podcast tracks sorting
    Former-commit-id: c54892d0d58fcbb33f6ad600d330b73283cc365f

--- app/serializers/podcast_serializer.rb
+++ app/serializers/podcast_serializer.rb
@@ -1,8 +1,9 @@
 class PodcastSerializer < ActiveModel::Serializer
-  attributes :id, :name
-  has_many :tracks, serializer: PlaylistTrackSerializer
+  attributes :id, :name, :tracks
 
   def tracks
-    object.playlist.playlist_tracks.order("podcast_published_date DESC")
+    object.playlist.playlist_tracks.unscoped.order("podcast_published_date DESC").map do |playlist_track|
+      PlaylistTrackSerializer.new(playlist_track, root: false)
+    end
   end
 end
