9373a18f67869b935f5985714ef02eaa5867d3c3
Tony Miller <mcfiredrill@gmail.com>
2016-06-16 21:51:34 +0900

    change save_recording since we don't need to grab the files from docker anymore
    Former-commit-id: e31fdc3863c8d640e7567afcc8c1871a82c04333

--- spec/services/save_recording_spec.rb
+++ spec/services/save_recording_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 describe SaveRecording do
-  xit "saves a new recording model given a path and radio" do
+  it "saves a new recording model given a path and radio" do
     radio = FactoryGirl.create :radio, name: "datafruits"
-    filename = "/home/liquidsoap/tracks/unhappy_supermarket_lektro.mp3"
-    FileUtils.copy "spec/fixtures/unhappy_supermarket_lektro.mp3", radio.tracks_directory
-    VCR.use_cassette "save_recording", :match_requests_on => [:method, :s3_uri_matcher] do
-      RadioBooter.boot radio
+    shinta = FactoryGirl.create :user, username: "shintaro.yonezawa"
+    filename = "/home/liquidsoap/tracks/datafruits/datafruits-LIVE -- shintaro.yonezawa - 05-30-2016, 12:12:02.mp3"
+    local_path = File.join(radio.tracks_directory, File.basename(filename))
+    FileUtils.copy "spec/fixtures/unhappy_supermarket_lektro.mp3", local_path
 
-      recording = SaveRecording.save filename, radio.name
-      expect(recording.file_file_size).to eq File.size(File.join(radio.tracks_directory, File.basename(filename)))
-    end
+    recording = SaveRecording.save filename, radio.name
+    expect(recording.file_file_size).to eq File.size(local_path)
+    expect(recording.dj).to eq shinta
+    expect(recording.radio).to eq radio
   end
 end
