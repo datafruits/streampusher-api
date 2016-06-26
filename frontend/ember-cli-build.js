ec30ce3ab22029d8751c185f3f62e323b1bdd83b
Tony Miller <mcfiredrill@gmail.com>
2016-06-26 22:13:15 +0900

    use fingerprint prepend but don't prepend script ember tags in prod
    Former-commit-id: c8ee69534e08e467c98625517da4cb600d5c05ef

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -5,9 +5,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
-    // fingerprint: {
-    //   prepend: '/assets/ember-cli/apps/frontend/'
-    // }
+    fingerprint: {
+      prepend: '/assets/ember-cli/apps/frontend/'
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
