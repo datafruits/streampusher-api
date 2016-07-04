9c7530a5c0cbe8d213507b5fa4828a1cc6ff8958
Tony Miller <mcfiredrill@gmail.com>
2016-07-04 12:05:08 +0900

    fix persist_playlist_to_redis
    Former-commit-id: 892eb0a7be34cd335c493b88624c96dc2d470322

--- app/services/persist_playlist_to_redis.rb
+++ app/services/persist_playlist_to_redis.rb
@@ -4,12 +4,12 @@ class PersistPlaylistToRedis
     @redis.del playlist.redis_key
     count = 0
     interpolated_playlist_count = 0
-    playlist.playlist_tracks.rank(:position).reverse.each do |playlist_track|
+    playlist.playlist_tracks.reverse.each do |playlist_track|
       track = Track.find(playlist_track.track_id)
       push_track playlist, track
       if playlist.interpolated_playlist_enabled?
         if count % playlist.interpolated_playlist_track_interval_count == 0
-          track = Track.find(playlist.interpolated_playlist.playlist_tracks.rank(:position).reverse[interpolated_playlist_count].track_id)
+          track = Track.find(playlist.interpolated_playlist.playlist_tracks.reverse[interpolated_playlist_count].track_id)
           push_track playlist, track
           interpolated_playlist_count += 1
           if interpolated_playlist_count >= playlist.interpolated_playlist.playlist_tracks.length
