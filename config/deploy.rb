3366fb0c05cced6c4326c302d8c416822315af04
Tony Miller <mcfiredrill@gmail.com>
2016-07-13 15:07:58 +0900

    set backup/.env in linked files
    Former-commit-id: 11eaa0a1859b4d5e47eb97026978baf7c1ff9402

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :deploy_user, "deploy"
 # set :pty, true
 #
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/application.yml backup/.env}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
