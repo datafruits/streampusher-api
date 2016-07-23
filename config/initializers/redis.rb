63fda5e3562935d2a94acda8afe8efa8dc11d2a3
Tony Miller <mcfiredrill@gmail.com>
2016-07-23 16:40:49 +0900

    redis passwords
    Former-commit-id: cbbfb823cbe0a99a05493592f60902f90a08486d

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
 host = ENV['REDIS_PORT_6379_TCP_ADDR'] || 'redis'
 port = ENV['REDIS_PORT_6379_TCP_PORT'] || 6379
-Redis.current = Redis.new host: host, port: port
+password = ENV['REDIS_PASSWORD']
+Redis.current = Redis.new host: host, port: port, password: password
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,11 @@
 host = ENV['REDIS_PORT_6379_TCP_ADDR'] || 'redis'
 port = ENV['REDIS_PORT_6379_TCP_PORT'] || 6379
+password = ENV['REDIS_PASSWORD']
 
 Sidekiq.configure_server do |config|
-  config.redis = { url:  "redis://#{host}:#{port}" }
+  config.redis = { url:  "redis://#{host}:#{port}", password: password  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url:  "redis://#{host}:#{port}" }
+  config.redis = { url:  "redis://#{host}:#{port}", password: password }
 end
