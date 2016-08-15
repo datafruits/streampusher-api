61ca0f7596d222a6441073657292b76df02e843f
Tony Miller <mcfiredrill@gmail.com>
2016-08-15 15:38:31 +0900

    personalized welcome email to be delivered 2 days after signup
    Stolen from patio11 of
    course
    https://customer.io/blog/What-are-Lifecycle-emails-patio11-patrick-mckenzie.html
    
    Former-commit-id: 16ee0edb8e0777e4ed8ed79bfd38cd4805693529

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -4,6 +4,11 @@ class AccountMailer < ApplicationMailer
     mail subject: "Welcome to Streampusher", to: @user.email
   end
 
+  def welcome_new_account_personalized user
+    @user = user
+    mail subject: "Anything I can help you with?", to: @user.email
+  end
+
   def trial_will_end user
     @user = user
     mail subject: "Your free trial on Streampusher is ending soon", to: @user.email
--- app/services/user_signed_up_notifier.rb
+++ app/services/user_signed_up_notifier.rb
@@ -2,6 +2,7 @@ class UserSignedUpNotifier
   def self.notify user
     AdminMailer.user_signed_up(user).deliver_later
     AccountMailer.welcome_new_account(user).deliver_later
+    AccountMailer.welcome_new_account_personalized(user).deliver_later wait: 2.days
     if ::Rails.env == "production"
       ga_event("user", "signup")
     end
