0f0dae433ab1fdc8a483ef04ecbdb484f931cf08
Tony Miller <mcfiredrill@gmail.com>
2016-08-18 13:45:07 +0900

    remove garails, seems outdated.
    Maybe will use another tool besides google analytics for server side
    analytics.
    
    
    Former-commit-id: 5566e45606ca5fcd86b4290002e705966413b5dd

--- app/services/user_signed_up_notifier.rb
+++ app/services/user_signed_up_notifier.rb
@@ -3,8 +3,5 @@ class UserSignedUpNotifier
     AdminMailer.user_signed_up(user).deliver_later
     AccountMailer.welcome_new_account(user).deliver_later
     AccountMailer.welcome_new_account_personalized(user).deliver_later wait: 2.days
-    if ::Rails.env == "production"
-      ga_event("user", "signup")
-    end
   end
 end
