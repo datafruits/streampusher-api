059ca0f97f4116ae20a413a8c771837a02728576
Tony Miller <mcfiredrill@gmail.com>
2016-08-25 08:57:04 +0900

    finish mixcloud service
    Former-commit-id: 5d729b65fc92e5e4bd92c85b313ccce1a9c1394e

--- app/services/upload_track_to_mixcloud.rb
+++ app/services/upload_track_to_mixcloud.rb
@@ -1,5 +1,13 @@
 class UploadTrackToMixcloud
   def perform track, mixcloud_token
-    Mixcloud.client.new(token).upload track.title
+    unless File.exists?(track.local_path)
+      DownloadTrackWorker.new.perform(track.id)
+    end
+    if track.artwork.present?
+      artwork = download_tempfile track.artwork.url
+      Mixcloud.client.new(token).upload track.local_path, track.title, artwork.path
+    else
+      Mixcloud.client.new(token).upload track.local_path, track.title
+    end
   end
 end
