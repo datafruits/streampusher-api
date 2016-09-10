1500045c17ec02339abb8bb591e7911041b24e48
Tony Miller <mcfiredrill@gmail.com>
2016-09-10 15:39:24 +0900

    fix paperclip initializer
    Former-commit-id: 83510e751c4e84b1b77a270a002b066a35da4387

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,11 +1,11 @@
 Paperclip::Attachment.default_options.merge!({
-  :storage => :s3,
+  storage: :s3,
   s3_credentials: {
     access_key_id: ENV['S3_KEY'],
     secret_access_key: ENV['S3_SECRET']
   },
   bucket: ENV['S3_BUCKET'],
   path: ":attachment/:style/:basename.:extension",
-    :url => ':s3_alias_url',
-    :s3_host_alias => ENV['CLOUDFRONT_URL']
+  url: ':s3_alias_url',
+  s3_host_alias: ENV['CLOUDFRONT_URL']
 })
