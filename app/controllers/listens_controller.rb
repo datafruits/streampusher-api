bff91d27ce82405e4450c0baeeda0bacef914919
Tony Miller <mcfiredrill@gmail.com>
2016-10-21 12:17:45 +0900

    sort listens by date in controller
    Former-commit-id: 71e9be5c38bcb7727ea0f3bf343b538cca065dab

--- app/controllers/listens_controller.rb
+++ app/controllers/listens_controller.rb
@@ -2,7 +2,7 @@ class ListensController < ApplicationController
   load_and_authorize_resource
   respond_to :json
   def index
-    @listens = Listen.group_by_day(:created_at).count
+    @listens = @current_radio.listens.group(:start_at).count
     render json: @listens
   end
 end
