7ee13f668f3068abb9b5802ca6728c41e8985e9f
Tony Miller <mcfiredrill@gmail.com>
2017-01-02 09:43:25 +0900

    massage metadata
    Former-commit-id: c1ec01533ef0b4b791844fe00fdf2d6cd19d9c4c

--- app/services/metadata_update.rb
+++ app/services/metadata_update.rb
@@ -1,5 +1,6 @@
 class MetadataUpdate
   def self.perform radio, metadata
+    metadata = massage_metadata metadata
     liquidsoap_socket = Liquidsoap::Socket.new(radio.liquidsoap_socket_path)
     liquidsoap_socket.write "metadata.update #{equalify_hash(metadata)}"
   end
@@ -7,5 +8,14 @@ class MetadataUpdate
   private
   def self.equalify_hash hash
     hash.map{|k,v| "#{k}=#{v}" }.join(",")
+  end
+
+  def self.massage_metadata metadata
+    [:artist, :album, :title].each do |key|
+      unless metadata.has_key? key
+        metadata[key] = " "
+      end
+    end
+    metadata
   end
 end
