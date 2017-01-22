d56012b643054560a3052c6389c066a261044510
Tony Miller <mcfiredrill@gmail.com>
2017-01-23 08:36:06 +0900

    fix spec
    Former-commit-id: 952a1eea2a143bf218a255074d6e250d22b15509

--- spec/services/collect_stats_spec.rb
+++ spec/services/collect_stats_spec.rb
@@ -24,20 +24,21 @@ describe CollectStats do
   end
 
   it "records the end_at if the listener is no longer connected" do
+    ICECAST_URL = "#{ENV['ICECAST_STATS_HOST']}/admin/listmounts?with_listeners"
     radio = FactoryGirl.create :radio, name: "datafruits"
     # two listeners connected
-    stub_request(:get, "http://localhost:8000/admin/listmounts?with_listeners").to_return(body: File.read("spec/fixtures/xml/two_listeners.xml"))
+    stub_request(:get, ICECAST_URL).to_return(body: File.read("spec/fixtures/xml/two_listeners.xml"))
     CollectStats.new(radio).perform
     expect(Listen.count).to eq 2
 
     # no listeners connected
-    stub_request(:get, "http://localhost:8000/admin/listmounts?with_listeners").to_return(body: File.read("spec/fixtures/xml/no_listeners.xml"))
+    stub_request(:get, ICECAST_URL).to_return(body: File.read("spec/fixtures/xml/no_listeners.xml"))
     end_time = Time.now
     Timecop.freeze(end_time)
     CollectStats.new(radio).perform
     expect(Listen.count).to eq 2
     Listen.all.each do |listen|
-      expect(listen.end_at).to eq end_time
+      expect(listen.end_at.to_i).to eq end_time.to_i
     end
     Timecop.return
   end
