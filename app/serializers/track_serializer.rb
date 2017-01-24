f7aefe58e6ec62d2ebe4ffaf9a1fb06d432cd5f0
Tony Miller <mcfiredrill@gmail.com>
2017-01-24 11:13:57 +0900

    add cdn_url to track_serializer
    Former-commit-id: 7a34dec7b76dd99220fb24c2c85db935a09b15cb

--- app/serializers/track_serializer.rb
+++ app/serializers/track_serializer.rb
@@ -1,7 +1,7 @@
 class TrackSerializer < ActiveModel::Serializer
   attributes :id, :audio_file_name, :display_name, :artist, :title, :album,
     :created_at, :artwork, :artwork_filename, :mixcloud_upload_status, :mixcloud_key,
-    :embed_link
+    :embed_link, :cdn_url
 
   def embed_link
     if ::Rails.env.production?
@@ -9,6 +9,10 @@ class TrackSerializer < ActiveModel::Serializer
     else
       "http://#{object.radio.virtual_host}:3000/tracks/#{object.id}/embed"
     end
+  end
+
+  def cdn_url
+    object.track.cdn_url
   end
 
   def mixcloud_upload_status
