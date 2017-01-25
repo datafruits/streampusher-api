64d32a844bcb45b54487e0ef02c8f8829369272c
Tony Miller <mcfiredrill@gmail.com>
2017-01-25 12:08:54 +0900

    begin to refactor player, fix spinning animation
    Former-commit-id: 4fbd17f6d85eaa96e3c74afb7ca6d001077de0f6

---
+++ app/assets/javascripts/player.js
@@ -0,0 +1,82 @@
+
+function Player(cssSelectorAncestor){
+  this.cssSelectorAncestor = cssSelectorAncestor;
+  this.radioTitle = function() {
+    var radioName, url;
+    url = $(".jp-jplayer").data('icecast-json').toString();
+    radioName = $(".jp-jplayer").data('radio-name').toString();
+    return $.get(url, function(data) {
+      var myRadio, title;
+      myRadio = data.icestats.source.find((function(_this) {
+	return function(s) {
+	  return s.server_name === (radioName + ".mp3");
+	};
+      })(this));
+      title = myRadio.title;
+      if($("#odometer").length){
+        var listeners = 0;
+        $.each(data.icestats.source, function(key, data){
+          listeners += data.listeners;
+        });
+        var counter = new countUp('odometer', 0, listeners, 0, 2.5);
+        counter.start();
+        console.log('listeners: '+listeners);
+      }
+      return $('.jp-title').html(title);
+    });
+  };
+
+  this.start = function() {
+    var mp3;
+    var stream = {
+      mp3: $(".jp-jplayer").data('mp3').toString()
+    }
+
+    var _this = this;
+    $("#jquery_jplayer_1").jPlayer({
+      ready: function() {
+	return $(this).jPlayer("setMedia", stream);
+      },
+      playing: function(e) {
+	return $('.jp-loading').hide();
+      },
+      pause: function(e) {
+        //if (_this.get('playingPodcast') === false) {
+          $(this).jPlayer("clearMedia");
+          $(this).jPlayer("setMedia", stream);
+        //}
+      },
+      error: function(event) {
+	console.log('jPlayer error: ' + event.jPlayer.error.type);
+	$('jp-pause').hide();
+	return $('jp-loading').hide();
+      },
+      waiting: function(e) {
+	$('.jp-loading').show();
+	$('.jp-play').hide();
+	return $('.jp-pause').hide();
+      },
+      loadeddata: function(e) {
+	return $('.jp-loading').hide();
+      },
+      cssSelectorAncestor: this.cssSelectorAncestor,
+      swfPath: "/assets/flash/jplayer",
+      supplied: "mp3",
+      useStateClassSkin: true,
+      autoBlur: false,
+      smoothPlayBar: true,
+      keyEnabled: true,
+      remainingDuration: true,
+      toggleDuration: true
+    });
+    setTimeout(function() {
+      return _this.radioTitle();
+    }, 500);
+
+    setInterval(function() {
+      return _this.radioTitle();
+    }, 10000);
+  };
+
+
+};
--- app/assets/javascripts/radios.js.coffee
+++ app/assets/javascripts/radios.js.coffee
@@ -1,68 +1,10 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
-#
-radioTitle = () ->
-  url = $(".jp-jplayer").data('icecast-json').toString()
-  radioName = $(".jp-jplayer").data('radio-name').toString()
-  console.log(url)
-
-  $.get url, (data) ->
-    myRadio = data.icestats.source.find (s) =>
-      s.server_name == "#{radioName}.mp3"
-    title = myRadio.title
-    $('.jp-title').html(title)
-    if $("#odometer").length
-      listeners = 0
-      $.each data.icestats.source, (key, data) ->
-        listeners += data.listeners
-        counter = new countUp('odometer', 0, listeners, 0, 2.5)
-        counter.start()
-      console.log('listeners: '+listeners)
+#= require player
 
 $(".jp-jplayer").ready ->
-  mp3 = $(".jp-jplayer").data('mp3').toString()
-  $("#jquery_jplayer_1").jPlayer({
-    ready: () ->
-      $(this).jPlayer("setMedia", {
-        mp3: mp3
-      })
-
-    playing: (e) ->
-      $('.jp-loading').hide()
-
-    error: (event) ->
-      console.log('jPlayer error: '+ event.jPlayer.error.type)
-
-      $('jp-pause').hide()
-      $('jp-loading').hide()
-
-    waiting: (e) ->
-      $('.jp-loading').show()
-      $('.jp-play').hide()
-      $('.jp-pause').hide()
-
-    loadeddata: (e) ->
-      $('.jp-loading').hide()
-
-    cssSelectorAncestor: "#navbar_jp_container",
-    swfPath: "/assets/flash/jplayer",
-    supplied: "mp3",
-    useStateClassSkin: true,
-    autoBlur: false,
-    smoothPlayBar: true,
-    keyEnabled: true,
-    remainingDuration: true,
-    toggleDuration: true
-  })
-
-  setTimeout () ->
-    radioTitle()
-  , 500
-
-  setInterval () ->
-    radioTitle()
-  , 10000
+  new Player("#navbar_jp_container").start()
 
 $('[data-controller=radios][data-action=index]').ready ->
   ctx = document.getElementById("listensChart").getContext("2d")
