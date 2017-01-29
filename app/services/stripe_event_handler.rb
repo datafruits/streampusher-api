206de8d7e385a0a71e7ded43297752e5b58103f0
Tony Miller <mcfiredrill@gmail.com>
2017-01-30 01:19:55 +0900

    openstruct is also unserializable, so use a hash
    Former-commit-id: 6426a4da499d5656a3c4670fe5eb31f0748ace92

--- app/services/stripe_event_handler.rb
+++ app/services/stripe_event_handler.rb
@@ -1,9 +1,9 @@
 class StripeEventHandler
   def self.customer_subscription_updated event
     user = Subscription.find_by!(stripe_customer_token: event.data.object.customer).user
-    old_plan = event.data.previous_attributes.plan
-    new_plan = event.data.object.plan
-    AccountMailer.subscription_updated(user).deliver_later
+    old_plan = event.data.previous_attributes.plan.id
+    new_plan = event.data.object.plan.id
+    AccountMailer.subscription_updated(user, old_plan, new_plan).deliver_later
   end
 
   def self.trial_will_end event
@@ -29,11 +29,11 @@ class StripeEventHandler
     # figure out if this is an ended free trial
     subscription = Subscription.find_by!(stripe_customer_token: event.data.object.customer)
     user = subscription.user
-    invoice = OpenStruct.new
-    invoice.currency = event.data.object.currency
-    invoice.amount = event.data.object.lines.data[0].amount
-    invoice.id = event.data.object.id
-    invoice.plan_id = event.data.object.lines.data[0].plan.id
+    invoice = {}
+    invoice[:currency] = event.data.object.currency
+    invoice[:amount] = event.data.object.lines.data[0].amount
+    invoice[:id] = event.data.object.id
+    invoice[:plan_id] = event.data.object.lines.data[0].plan.id
 
     AccountMailer.invoice(user, invoice).deliver_later
   end
