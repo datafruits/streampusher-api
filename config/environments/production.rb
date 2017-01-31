99edcaffc24d3aa5f5f5163b71e588b6d3354773
Tony Miller <mcfiredrill@gmail.com>
2017-01-31 10:26:59 +0900

    set action_mailer.asset_host in production
    Former-commit-id: dc1914ca01ec75a80751d44a693e30e68440f22b

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
+  config.action_mailer.asset_host = "https://streampusher.com"
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
