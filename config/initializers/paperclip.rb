468ecc5a9ff85c3a6a1d84e5fceb64e8a4e806a3
Tony Miller <mcfiredrill@gmail.com>
2017-02-24 12:52:27 +0900

    default ssl for paperclip s3 storage
    Former-commit-id: 1613fb4a4dddb438e19bd275701657a1ecc9ba91

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,6 +4,7 @@ Paperclip::Attachment.default_options.merge!({
     access_key_id: ENV['S3_KEY'],
     secret_access_key: ENV['S3_SECRET']
   },
+  s3_protocol: :https,
   bucket: ENV['S3_BUCKET'],
   path: ":attachment/:style/:basename.:extension",
   url: ':s3_alias_url',
