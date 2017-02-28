bcb84dba185e4ba4f653d70340b110e81998fdb6
Tony Miller <mcfiredrill@gmail.com>
2017-02-28 14:30:23 +0900

    add coupon param to subscriptions_controller and instrument updates
    Former-commit-id: 3e2cb17466180cf58e60ab120d05d50e82f13cee

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -7,6 +7,7 @@ class SubscriptionsController < ApplicationController
   def update
     if @subscription.update_with_new_card create_params
       flash[:notice] = "Updated your subscription successfully."
+      ActiveSupport::Notifications.instrument 'subscription.updated', user: current_user.email, radio: @current_radio.name, subscription: @subscription.inspect
       redirect_to edit_subscription_path @subscription
     else
       flash[:error] = "Error updating your subscription."
@@ -16,6 +17,6 @@ class SubscriptionsController < ApplicationController
 
   private
   def create_params
-    params.require(:subscription).permit(:plan_id, :stripe_card_token)
+    params.require(:subscription).permit(:plan_id, :stripe_card_token, :coupon)
   end
 end
