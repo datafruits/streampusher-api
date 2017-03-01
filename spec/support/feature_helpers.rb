9d79a6f3fd8a2de45d4be2f81e9a16036a3bb47a
Tony Miller <mcfiredrill@gmail.com>
2017-03-01 19:56:48 +0900

    remove expectation because flash message can disappear too fast sometimes
    Former-commit-id: 1e1d50cbebc8f17362617ea69dcf741df3467ab1

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -4,7 +4,6 @@ module FeatureHelpers
     fill_in "user_login", with: user.email
     fill_in "user_password", with: "password"
     click_button "Sign in"
-    expect(page).to have_content "Signed in successfully."
   end
 
   def select_date(date, options = {})
