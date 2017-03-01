5ed1c2a2b9f22307915672065e7e9279c95fa100
Tony Miller <mcfiredrill@gmail.com>
2017-03-01 12:22:11 +0900

    woops fix script
    Former-commit-id: f669a660eac2aff0b46366ce9efbea8f6ce2e174

--- script/ci.sh
+++ script/ci.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-bundle exec rake spec
 eval "$(rbenv init -)"
 echo $PATH
 cp config/database.ci.yml config/database.yml
@@ -7,3 +6,4 @@ bundle install --jobs $(nproc)
 RAILS_ENV=test bundle exec rake db:create
 RAILS_ENV=test bundle exec rake db:migrate
 RAILS_ENV=test bundle exec rake ember:install
+bundle exec rake spec
