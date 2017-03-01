f6828bfbbb15e49b721bc3fe2f5c1409879f32a5
Tony Miller <mcfiredrill@gmail.com>
2017-03-01 10:53:38 +0900

    add actions to recordings_controller
    Former-commit-id: 287d3c378398e12df3e31edc2274e1c3a7ba8a4c

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -3,4 +3,13 @@ class RecordingsController < ApplicationController
   def index
     @recordings = @current_radio.recordings
   end
+
+  def create
+    @recording = current_radio.recordings.new recording_params
+  end
+
+  private
+  def recording_params
+    params.require(:recording).permit(:audio_file_name)
+  end
 end
