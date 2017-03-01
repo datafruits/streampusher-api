212b7e9be3e041fd4d4a0b1a4679cd88aa29cd77
Tony Miller <mcfiredrill@gmail.com>
2017-03-01 10:53:46 +0900

    metadata_update_spec
    Former-commit-id: c8c2aabd079954db7bbf3f357f532cdf2654a800

--- spec/services/metadata_update_spec.rb
+++ spec/services/metadata_update_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 require 'sidekiq/testing'
 
 describe MetadataUpdate do
-  let(:radio){ FactoryGirl.create :radio }
+  let(:radio){ double }
   it "sends metadata update command to liquidsoap" do
+    radio.stub(:liquidsoap_socket_path)
     metadata = { :artist => "tony", :title => "hey", :album => "my album" }
     MetadataUpdate.perform radio, metadata
   end
