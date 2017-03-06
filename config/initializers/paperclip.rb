fc734df94fc9be5296603c68dd9148818e943bee
Tony Miller <mcfiredrill@gmail.com>
2017-03-06 16:37:25 +0900

    add S3_REGION for paperclip
    Former-commit-id: 0ef856b6c5be95a17470f1271494b83a50d59bd2

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,7 +2,8 @@ Paperclip::Attachment.default_options.merge!({
   storage: :s3,
   s3_credentials: {
     access_key_id: ENV['S3_KEY'],
-    secret_access_key: ENV['S3_SECRET']
+    secret_access_key: ENV['S3_SECRET'],
+    s3_region: ENV['S3_REGION']
   },
   s3_protocol: :https,
   bucket: ENV['S3_BUCKET'],
