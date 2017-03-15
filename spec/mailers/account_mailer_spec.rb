ef056b85b47f60ee96787ebe6459dd55b1f6fd0f
Tony Miller <mcfiredrill@gmail.com>
2017-03-15 13:26:37 +0900

    fix invoice mail template
    Former-commit-id: 12280e10f82eccd5c9c298f7961c34955caf64c8

--- spec/mailers/account_mailer_spec.rb
+++ spec/mailers/account_mailer_spec.rb
@@ -1,5 +1,10 @@
 require "rails_helper"
 
 RSpec.describe AccountMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user){ FactoryGirl.create :user, subscription: FactoryGirl.create(:subscription) }
+  let(:invoice){ { amount: "1900", currency: "USD" } }
+  it "sends invoice mail" do
+    mail = AccountMailer.invoice(user, invoice).deliver_now
+    puts mail.body
+  end
 end
