e089cdbb19132422cf52028e738a13441fad87a9
Tony Miller <mcfiredrill@gmail.com>
2017-03-18 13:54:07 +0900

    use https in download_tempfile
    Former-commit-id: 2c2137aa8c5c1be1e104966a1621d70d2960dc0f

--- lib/download_tempfile.rb
+++ lib/download_tempfile.rb
@@ -4,7 +4,7 @@ require 'uri'
 
 def download_tempfile(url)
   uri = URI.parse(url)
-  Net::HTTP.start(uri.host, uri.port) do |http|
+  Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
     resp = http.get(uri.path)
     file = Tempfile.new(File.basename(url))
     file.binmode
