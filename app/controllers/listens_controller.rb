9c843f4ce29e9018c9accd7ef72f3f992dfcf2b8
Tony Miller <mcfiredrill@gmail.com>
2017-05-04 22:31:00 +0900

    group listens by day using date_trunc
    Former-commit-id: 46520df1b6ca1975a92ed118163fe1667586ddd6

--- app/controllers/listens_controller.rb
+++ app/controllers/listens_controller.rb
@@ -2,7 +2,7 @@ class ListensController < ApplicationController
   load_and_authorize_resource
   respond_to :json
   def index
-    @listens = @current_radio.listens.group(:start_at).count
+    @listens = @current_radio.listens.group("date_trunc('day',start_at)").order("count_all ASC").count
     render json: @listens
   end
 end
