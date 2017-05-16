8d9dc8c5bc79cb5ef1b14448b653833b7fc4f29c
Tony Miller <mcfiredrill@gmail.com>
2017-05-16 17:55:04 +0900

    use insecure redis if set
    Former-commit-id: 249546130c549284d11a99101a000a8cc73be37a

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,13 +1,20 @@
+if ENV['INSECURE_REDIS']
+  redis = Redis.new
+else
+  redis = Redis.new(password: ENV['REDIS_PASSWORD'])
+end
+
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
   # lookup: :google,            # name of geocoding service (symbol)
+  ip_lookup: :freegeoip,
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
   # api_key: nil,               # API key for geocoding service
-  cache: Redis.new(password: ENV['REDIS_PASSWORD']),                 # cache object (must respond to #[], #[]=, and #keys)
+  cache: redis,                 # cache object (must respond to #[], #[]=, and #keys)
   cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
   # Exceptions that should not be rescued by default
