5ce06dd3d71dd3ce498b67b8bf42b0a38d377486
Tony Miller <mcfiredrill@gmail.com>
2017-05-19 12:18:17 +0900

    discount might not be present
    Former-commit-id: f5155e700c4a6e1a50c90406f227913b79e9f224

--- app/services/stripe_event_handler.rb
+++ app/services/stripe_event_handler.rb
@@ -48,7 +48,7 @@ class StripeEventHandler
       invoice[:amount_due] = event.data.object.amount_due
       invoice[:id] = event.data.object.id
       invoice[:plan_id] = event.data.object.lines.data[0].plan.id
-      if event.data.object.discount.coupon
+      if event.data.object.discount.try(:coupon)
         invoice[:coupon] = Hash(event.data.object.discount.coupon)
       end
       AccountMailer.invoice(user, invoice).deliver_later
