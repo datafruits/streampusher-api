ef6de9b5568316e74d8cdac576aac4cf62db68b9
Tony Miller <mcfiredrill@gmail.com>
2017-05-26 01:14:31 +0900

    add createTag action
    Former-commit-id: 7581a8f97786e044b381b042038d8649c1f64e0f

--- frontend/app/components/track-labels-select.js
+++ frontend/app/components/track-labels-select.js
@@ -1,8 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
   actions: {
-    createTag(){
+    createTag(name){
+      let store = this.get('store');
+      let label = store.createRecord('label', { name: name });
+      let onSuccess = () =>{
+        console.log("label saved!");
+      };
+      let onFail = () => {
+        console.log("label save failed");
+      };
+      label.save().then(onSuccess, onFail);
     }
   }
 });
