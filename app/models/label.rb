3f4942820033c4722f253ba9256de9bce53cf86e
Tony Miller <mcfiredrill@gmail.com>
2017-05-26 11:24:48 +0900

    label should be unique per radio
    Former-commit-id: dd416f8e50a9bf9ac9ff4e9c917e02c95678ffa1

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,5 +1,5 @@
 class Label < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { scope: :radio_id }
   has_many :track_labels, dependent: :destroy
   has_many :tracks, through: :track_labels
   belongs_to :radio
