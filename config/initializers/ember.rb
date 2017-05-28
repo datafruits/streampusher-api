79e3d11283063ee106168aee180f95fb3a32e79a
Tony Miller <mcfiredrill@gmail.com>
2017-05-28 13:45:33 +0900

    use yarn
    Former-commit-id: 1f91e2931bd92ae61acf2290393e5553622c55f4

--- config/initializers/ember.rb
+++ config/initializers/ember.rb
@@ -1,5 +1,5 @@
 # config/intializers/ember.rb
 EmberCli.instance_variable_set(:@root, Rails.root.join("public/assets/ember-cli").tap(&:mkpath))
 EmberCli.configure do |c|
-   c.app :frontend, exclude_ember_deps: "jquery"
+   c.app :frontend, exclude_ember_deps: "jquery", yarn: true
 end
