0dd64f5c118114dfd5712ac31a69a71301852674
Tony Miller <mcfiredrill@gmail.com>
2017-06-13 10:41:48 +0900

    spec for setting username from email
    Former-commit-id: f8589faffee641c3c63981e9aa72bf3e48466ec4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   let(:user){ User.create username: "mcfiredrill", time_zone: "Tokyo", role: "admin owner" }
+  describe "username" do
+    it "is set from email if no username is present" do
+      user = User.create email: "mcfiredrill@gmail.com", time_zone: "Tokyo", role: "admin owner"
+      expect(user.username).to eq "mcfiredrill"
+    end
+  end
+
   describe "roles" do
     it "adds a new role" do
       user.add_role "dj"
