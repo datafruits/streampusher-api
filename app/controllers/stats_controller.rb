7171fc4949c29257b290c89ea269ffbefa15cedb
Tony Miller <mcfiredrill@gmail.com>
2017-06-16 14:57:30 -0700

    wip
    Former-commit-id: c972b4bcd4a1d833c005f9a28e04b0530d78d1f2

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,6 @@
 class StatsController < ApplicationController
   def index
     authorize! :index, :stats
+    @listens = @current_radio.listens.order("date_trunc('day',start_at) ASC").group("date_trunc('day', start_at)").count
   end
 end
--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,2 +1,9 @@
 module StatsHelper
+  def average_sessions_per_hour listens
+
+  end
+
+  def average_listening_minutes_per_session listens
+
+  end
 end
