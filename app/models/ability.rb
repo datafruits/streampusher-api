c7e350adda4a66df7130f40f1809e60ad7065abe
Tony Miller <mcfiredrill@gmail.com>
2017-07-18 12:55:38 +0900

    fix permissions for dj index
    Former-commit-id: adcd6593cd711d395e8250cb1a55ae202956b4ae

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -104,6 +104,7 @@ class Ability
       cannot :admin
     else
       can :read, ScheduledShow if format == "json"
+      can :index, :dj if format == "json"
       can :read, :dj if format == "json"
       can :next, ScheduledShow if format == "json"
       can :enabled, :vj
