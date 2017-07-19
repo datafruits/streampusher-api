47838a80b9aac394cd5fdddc771f5dee3847073e
Tony Miller <mcfiredrill@gmail.com>
2017-07-19 18:39:09 +0900

    wip
    Former-commit-id: 350180254000632dea5477a3a1baaf0f1f28feba

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -9,6 +9,26 @@ class AccountMailer < ApplicationMailer
     mail subject: "Anything I can help you with?", to: @user.email
   end
 
+  def playlists user
+    @user = user
+    mail subject: "Get started with Streampusher playlists", to: @user.email
+  end
+
+  def stats user
+    @user = user
+    mail subject: "Know your audience with Streampusher stats", to: @user.email
+  end
+
+  def djs user
+    @user = user
+    mail subject: "Assign other DJs to your station", to: @user.email
+  end
+
+  def broadcasting user
+    @user = user
+    mail subject: "Have you broadcasted live yet?", to: @user.email
+  end
+
   def trial_will_end user
     @user = user
     mail subject: "Your free trial on Streampusher is ending soon", to: @user.email
@@ -17,6 +37,11 @@ class AccountMailer < ApplicationMailer
   def trial_ended user
     @user = user
     mail subject: "Your free trial on Streampusher has ended", to: @user.email
+  end
+
+  def day_after_trial_ended user
+    @user = user
+    mail subject: "Can I offer you a discount?", to: @user.email
   end
 
   def subscription_updated user, old_plan, new_plan
--- app/services/user_signed_up_notifier.rb
+++ app/services/user_signed_up_notifier.rb
@@ -3,5 +3,10 @@ class UserSignedUpNotifier
     AdminMailer.user_signed_up(user).deliver_later
     AccountMailer.welcome_new_account(user).deliver_later
     AccountMailer.welcome_new_account_personalized(user).deliver_later wait: 2.days
+    AccountMailer.playlists(user).deliver_later wait: 3.days
+    AccountMailer.broadcasting(user).deliver_later wait: 4.days
+    AccountMailer.djs(user).deliver_later wait: 5.days
+    AccountMailer.stats(user).deliver_later wait: 6.days
+    AccountMailer.day_after_trial_ended(user).deliver_later wait: 8.days
   end
 end
