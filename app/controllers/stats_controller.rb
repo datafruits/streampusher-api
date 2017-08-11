d1d0a1591a3b09524de2adb970bd0ae6e1d395fc
Tony Miller <mcfiredrill@gmail.com>
2017-08-11 14:12:22 +0900

    only use distinct for grouped by data
    Former-commit-id: f9f7a9f218a7f9632d76967ec60e5d33f214a17c

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,10 +3,10 @@ class StatsController < ApplicationController
     authorize! :index, :stats
     @start_at = params[:start_at] || 7.days.ago
     @end_at = params[:end_at] || Time.now
+
     @listens = @current_radio.listens
       .where("start_at >= (?) AND end_at <= (?)", @start_at, @end_at)
-      .select(:ip_address).distinct
-    @listens_by_day = @listens.group_by_day(:start_at).count
-    @listens_by_hour = @listens.group_by_hour(:start_at).count
+    @listens_by_day = @listens.select(:ip_address).distinct.group_by_day(:start_at).count
+    @listens_by_hour = @listens.select(:ip_address).distinct.group_by_hour(:start_at).count
   end
 end
