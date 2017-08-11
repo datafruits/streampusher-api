914516326528410f300d4009f8124c3f55809953
Tony Miller <mcfiredrill@gmail.com>
2017-08-11 13:36:30 +0900

    embed json in dom
    Former-commit-id: 207a02ace3b1c7de806a4f7809fbfd233b95cc56

--- app/controllers/listens_controller.rb
+++ app/controllers/listens_controller.rb
@@ -5,6 +5,7 @@ class ListensController < ApplicationController
   def index
     @start_at = params[:start_at] || 7.days.ago
     @end_at = params[:end_at] || Time.now
+    binding.pry
     @listens = @current_radio.listens
       .where("start_at >= (?) AND end_at <= (?)", @start_at, @end_at)
       .select(:ip_address).distinct.group_by_day(:start_at).count
