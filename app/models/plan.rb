32f91563e6630118b0dc8676f7ceca51e07cb2ab
Tony Miller <mcfiredrill@gmail.com>
2017-09-08 11:34:40 +0900

    show plan price on checkout page
    Former-commit-id: 837db9358393956ad63f496bfca1504f87972649

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,3 +1,8 @@
 class Plan < ActiveRecord::Base
   has_paper_trail
+  include ActionView::Helpers::NumberHelper
+
+  def name_and_price
+    "#{self.name} - #{number_to_currency(self.price)}"
+  end
 end
