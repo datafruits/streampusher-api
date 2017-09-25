5420dfe677eeec63e720a24d6f975007f58d17e4
Tony Miller <mcfiredrill@gmail.com>
2017-09-25 12:29:25 +0900

    don't fix old scheduled shows data for now
    Former-commit-id: ba447cfb0ae6e82d8ea028f6b2d471541cc5d1ac

--- db/migrate/20170922083418_add_slugs_to_scheduled_shows.rb
+++ db/migrate/20170922083418_add_slugs_to_scheduled_shows.rb
@@ -1,8 +1,5 @@
 class AddSlugsToScheduledShows < ActiveRecord::Migration[5.0]
   def change
     add_column :scheduled_shows, :slug, :string
-    ScheduledShow.find_each do |scheduled_show|
-      scheduled_show.save!
-    end
   end
 end
