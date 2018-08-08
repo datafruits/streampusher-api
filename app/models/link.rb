c5b75b9b2c276d2eb966fa5f8fc61efa10dcad34
Tony Miller <mcfiredrill@gmail.com>
2018-08-08 12:15:00 +0900

    failing link spec
    Former-commit-id: 013ddb493393c70ab5e2ad116d4164092d67ff83

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,7 @@
 class Link < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :url
+
+  def glyph
+  end
 end
---
+++ spec/models/link_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Link, type: :model do
+  it "determines the font awesome character to use from the url" do
+    expect(Link.new(url: "soundcloud.com/firedrill")).glyph .to eq "soundcloud"
+  end
+end
