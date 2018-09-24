ea5ddd91b0fe4af362098aeee1fb0ddfeb10ffbe
Tony Miller <mcfiredrill@gmail.com>
2018-09-24 10:53:13 +0900

    allow any origin :grimacing:
    Former-commit-id: 19cd40e9225c59a16c9f76232606432f03290c12

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module StreamPusher
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://datafruits.fm'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
