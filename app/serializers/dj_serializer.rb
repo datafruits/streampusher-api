0dfdf10c3767d043445e1aaa0cdbfd86f43f7b3a
Tony Miller <mcfiredrill@gmail.com>
2018-09-27 21:18:25 +0900

    add podcasts to dj_serializer
    Former-commit-id: 761056bb69915dee4125812a8e94e2b90af1b367

--- app/serializers/dj_serializer.rb
+++ app/serializers/dj_serializer.rb
@@ -1,5 +1,7 @@
 class DjSerializer < ActiveModel::Serializer
   attributes :id, :username, :image_url, :bio
+  has_many :links, embed: :ids, embed_in_root: true, each_serializer: LinkSerializer
+  has_many :tracks, embed: :ids, key: :tracks, embed_in_root: true, each_serializer: TrackSerializer
 
   def image_url
     if object.image.present?
@@ -7,7 +9,13 @@ class DjSerializer < ActiveModel::Serializer
     end
   end
 
-  def podcasts
-    Track.find(ScheduledShow.find(ScheduledShowPerformer.where(user_id: object.id).pluck(:scheduled_show_id)).map(&:track_ids)).order("created_at DESC")
+  def tracks
+    track_ids = ScheduledShow.find(ScheduledShowPerformer.where(user_id: object.id)
+      .pluck(:scheduled_show_id)).map(&:track_ids).flatten
+    if track_ids.any?
+      Track.find(track_ids) #.order("created_at DESC")
+    else
+      []
+    end
   end
 end
