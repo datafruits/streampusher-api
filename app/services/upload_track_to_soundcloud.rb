50a94a52f326008a50f3b7d78508b9cc13afd764
Tony Miller <mcfiredrill@gmail.com>
2018-11-22 16:50:51 +0900

    fix upload_track_to_soundcloud
    Former-commit-id: ee6d9d5db91c483cecd8d2645b05fca675ea437a

--- app/services/upload_track_to_soundcloud.rb
+++ app/services/upload_track_to_soundcloud.rb
@@ -12,23 +12,23 @@ class UploadTrackToSoundcloud
     if track.artwork.present?
       artwork = download_tempfile track.artwork.url
       # upload a new track with audio.mp3 as audio and image.jpg as artwork
-      track = client.post('/tracks', :track => {
+      result = client.post('/tracks', :track => {
         :title      => track.title,
         :asset_data => File.new(track.local_path),
         :artwork_data => File.new(artwork.path),
         :tags => track.labels.pluck(:name).join(" ")
       })
     else
-      track = client.post('/tracks', :track => {
+      result = client.post('/tracks', :track => {
         :title      => track.title,
         :asset_data => File.new(track.local_path)
       })
     end
 
     # print new tracks link
-    puts track.permalink_url
+    puts result.permalink_url
     track.soundcloud_upload_status = "soundcloud_upload_complete"
-    track.soundcloud_key = track.permalink_url
+    track.soundcloud_key = result.permalink_url
     track.save(validate: false)
   end
 end
