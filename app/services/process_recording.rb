f5b1b33cb41b35a048c59a6db08755ded73ed310
Tony Miller <mcfiredrill@gmail.com>
2019-02-13 22:49:33 +0900

    rescue processing failures
    Former-commit-id: bc1cd9256860e3123f4da437c0f093880038e84b

--- app/services/process_recording.rb
+++ app/services/process_recording.rb
@@ -4,28 +4,33 @@ class ProcessRecording
   def perform recording, scheduled_show: nil
     recording.update processing_status: 'processing'
 
-    trimmed_file_path = Sox.trim recording.path
+    begin
+      trimmed_file_path = Sox.trim recording.path
 
-    radio = recording.radio
-    s3_client = Aws::S3::Client.new
-    bucket = ENV['S3_BUCKET']
-    artist_name = File.basename(recording.path).split("LIVE -- ").last.split(" - ").first
-    date = File.basename(recording.path).split("LIVE -- ").last.split(" - ").last.split(", ").first
-    basename = "datafruits-#{artist_name}-#{date}.mp3"
-    key = "#{radio.name}/#{basename}"
-    content_type = "audio/mpeg"
+      radio = recording.radio
+      s3_client = Aws::S3::Client.new
+      bucket = ENV['S3_BUCKET']
+      artist_name = File.basename(recording.path).split("LIVE -- ").last.split(" - ").first
+      date = File.basename(recording.path).split("LIVE -- ").last.split(" - ").last.split(", ").first
+      basename = "datafruits-#{artist_name}-#{date}.mp3"
+      key = "#{radio.name}/#{basename}"
+      content_type = "audio/mpeg"
 
-    response = s3_client.put_object bucket: bucket, key: key,
-      acl: "public-read",
-      body: File.open(trimmed_file_path),
-      content_type: content_type
-    audio_file_name = "https://#{ENV['S3_BUCKET']}.s3.amazonaws.com/#{key}"
-    track = radio.tracks.create audio_file_name: audio_file_name
+      response = s3_client.put_object bucket: bucket, key: key,
+        acl: "public-read",
+        body: File.open(trimmed_file_path),
+        content_type: content_type
+      audio_file_name = "https://#{ENV['S3_BUCKET']}.s3.amazonaws.com/#{key}"
+      track = radio.tracks.create audio_file_name: audio_file_name
 
-    # if scheduled_show.present?
-    #   # grab scheduled show's info to use metadata and artwork
-    #   track.fill_metadata_from_show
-    # end
+      # if scheduled_show.present?
+      #   # grab scheduled show's info to use metadata and artwork
+      #   track.fill_metadata_from_show
+      # end
+    rescue Exception => e
+      recording.update processing_status: 'processing_failed'
+      raise e
+    end
     recording.update processing_status: 'processed', track: track
   end
 end
