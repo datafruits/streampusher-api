4f063c267d177b1bfe8acdf579a9e6f22e65cb2e
Tony Miller <mcfiredrill@gmail.com>
2019-05-17 17:54:14 +0900

    add dependent destroy option to ScheduledShowPerformer's belongs_to scheduled_show
    Former-commit-id: 82a16afe92d0df16f5b572d0fa9e6f0bb343c702

--- app/models/scheduled_show_performer.rb
+++ app/models/scheduled_show_performer.rb
@@ -1,5 +1,5 @@
 class ScheduledShowPerformer < ActiveRecord::Base
   belongs_to :user
-  belongs_to :scheduled_show
+  belongs_to :scheduled_show, dependent: :destroy
   validates :user, uniqueness: { scope: :scheduled_show, message: "You can't assign the same user multiple times to the same scheduled show" }
 end
