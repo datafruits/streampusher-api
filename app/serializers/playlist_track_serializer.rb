d258df111cb2c26c1604299b7a85a227297f70fe
Tony Miller <mcfiredrill@gmail.com>
2019-06-19 00:36:07 +0900

    add scheduled_show_id to playlist_track_serializer
    Former-commit-id: 4f9eafaf076e98ffbba1df5ce27d71e9e3e1a2f4

--- app/serializers/playlist_track_serializer.rb
+++ app/serializers/playlist_track_serializer.rb
@@ -1,8 +1,12 @@
 class PlaylistTrackSerializer < ActiveModel::Serializer
   attributes :audio_file_name, :podcast_published_date, :id,
     :track_id, :playlist_id, :title, :display_name, :position, :updated_at,
-    :cdn_url, :labels
+    :cdn_url, :labels, :scheduled_show_id
   has_many :labels, embed: :ids, key: :labels, embed_in_root: true
+
+  def scheduled_show_id
+    object.track.scheduled_show_id
+  end
 
   def display_name
     object.track.display_name
