41b1a71922292e71a5a4a9cf032585fdd8819936
Tony Miller <mcfiredrill@gmail.com>
2019-06-22 23:53:16 +0900

    dont enqueue unnecessary update_all_recurrences jobs
    check update_all_recurrences? before enqueueing the job
    
    
    Former-commit-id: e271736b1eb8491b158c39aa8e2eecfb2cf0950d

--- app/workers/update_recurring_shows_worker.rb
+++ app/workers/update_recurring_shows_worker.rb
@@ -1,9 +1,8 @@
 class UpdateRecurringShowsWorker < ActiveJob::Base
   queue_as :default
 
-  def perform scheduled_show_id, update_all_recurrences
+  def perform scheduled_show_id
     scheduled_show = ScheduledShow.find scheduled_show_id
-    scheduled_show.update_all_recurrences = ActiveModel::Type::Boolean.new.cast(update_all_recurrences)
     scheduled_show.update_recurrences
   end
 end
