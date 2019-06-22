287b7b4a1d92394a7a1eabd911cf4a25e4858315
Tony Miller <mcfiredrill@gmail.com>
2019-06-22 23:46:08 +0900

    cast update_all_recurrences to bool
    Former-commit-id: 4180237a091b26b587902b4455a2d2ffa1d3d569

--- app/workers/update_recurring_shows_worker.rb
+++ app/workers/update_recurring_shows_worker.rb
@@ -3,7 +3,7 @@ class UpdateRecurringShowsWorker < ActiveJob::Base
 
   def perform scheduled_show_id, update_all_recurrences
     scheduled_show = ScheduledShow.find scheduled_show_id
-    scheduled_show.update_all_recurrences = update_all_recurrences
+    scheduled_show.update_all_recurrences = ActiveModel::Type::Boolean.new.cast(update_all_recurrences)
     scheduled_show.update_recurrences
   end
 end
