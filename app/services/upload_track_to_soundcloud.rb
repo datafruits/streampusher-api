ac08518b2ff5c3692c544952f113882f640e04a3
Tony Miller <mcfiredrill@gmail.com>
2019-07-22 15:55:31 +0900

    rescue SoundCloud::ResponseError in souncloud uploader
    Former-commit-id: bb3015a3aa0995bbca7c5073abab8f30fa03bdfa

--- app/services/upload_track_to_soundcloud.rb
+++ app/services/upload_track_to_soundcloud.rb
@@ -9,21 +9,28 @@ class UploadTrackToSoundcloud
     end
     client = SoundCloud.new(:access_token => soundcloud_token)
 
-    if track.artwork.present?
-      artwork = download_tempfile track.artwork.url
-      # upload a new track with audio.mp3 as audio and image.jpg as artwork
-      result = client.post('/tracks', :track => {
-        :title      => track.title,
-        :asset_data => File.new(track.local_path),
-        :artwork_data => File.new(artwork.path),
-        :genre => track.labels.pluck(:name).first,
-        :tag_list => track.labels.pluck(:name).drop(1).join(" ")
-      })
-    else
-      result = client.post('/tracks', :track => {
-        :title      => track.title,
-        :asset_data => File.new(track.local_path)
-      })
+    begin
+      if track.artwork.present?
+        artwork = download_tempfile track.artwork.url
+        # upload a new track with audio.mp3 as audio and image.jpg as artwork
+        result = client.post('/tracks', :track => {
+          :title      => track.title,
+          :asset_data => File.new(track.local_path),
+          :artwork_data => File.new(artwork.path),
+          :genre => track.labels.pluck(:name).first,
+          :tag_list => track.labels.pluck(:name).drop(1).join(" ")
+        })
+      else
+        result = client.post('/tracks', :track => {
+          :title      => track.title,
+          :asset_data => File.new(track.local_path)
+        })
+      end
+    rescue SoundCloud::ResponseError => e
+      puts "soundcloud response error: #{e}"
+      track.soundcloud_upload_status = "soundcloud_upload_failed"
+      track.save(validate: false)
+      return
     end
 
     # print new tracks link
