3ae42f98ab1b53b38e8f5dec0cfe3b4a72cce7bc
Tony Miller <mcfiredrill@gmail.com>
2019-10-17 23:15:36 +0900

    protect publish_metadata endpoint
    Former-commit-id: 36753785ecf0fb5a9ef0a64a79f207a5741abc80

--- app/controllers/publish_metadata_controller.rb
+++ app/controllers/publish_metadata_controller.rb
@@ -1,7 +1,18 @@
 class PublishMetadataController < ApplicationController
   before_action :current_radio_required
   def create
-    MetadataPublisher.perform @current_radio.name, params[:metadata]
-    head :ok
+    if liq_authorized?
+      render json: "not permitted", status: :unauthorized
+    else
+      MetadataPublisher.perform @current_radio.name, params[:metadata]
+      head :ok
+    end
+  end
+
+  private
+
+  def liq_authorized?
+    liq_secret = request.headers["liq-secret"]
+    return liq_secret == Rails.application.secrets.liq_secret
   end
 end
