f966f64f17b463b2229993dc5a0a02f035f8399c
Tony Miller <mcfiredrill@gmail.com>
2019-11-19 16:33:26 +0900

    use aws sdk signer v4
    Former-commit-id: 3f93bf2a53caa69c3c4adc685a6a7a2e663b9698

--- app/controllers/uploader_signature_controller.rb
+++ app/controllers/uploader_signature_controller.rb
@@ -1,4 +1,7 @@
+require 'aws-sigv4'
+
 class UploaderSignatureController < ApplicationController
+  before_action :current_radio_required
   def index
     client = Aws::S3::Client.new(
       :region => 'us-east-1',
@@ -6,13 +9,28 @@ class UploaderSignatureController < ApplicationController
       :secret_access_key => ENV['S3_SECRET'],
     )
     content_type = correct_mime_type
-    signer = Aws::S3::Presigner.new client: client
-    url = signer.presigned_url(:put_object, bucket: ENV['S3_BUCKET'],
-                                            key: "#{@current_radio.container_name}/#{cleaned_filename(params[:name])}",
-                                            expires_in: 10.hours.to_i,
-                                            acl: "public-read",
-                                            content_type: content_type)
-    render json: { endpoint: url }
+
+    signer = Aws::Sigv4::Signer.new(
+                                    service: 's3',
+                                    region: 'us-east-1',
+                                    access_key_id: ENV['S3_KEY'],
+                                    secret_access_key: ENV['S3_SECRET'],
+                                    uri_escape_path: false,
+                                   )
+
+    # create presigned url for an object with bucket 'a-fancy-bucket' and key 'hello_world'
+    url = signer.presign_url(
+                             http_method: 'PUT',
+                             url: "https://#{ENV['S3_BUCKET']}.s3.amazonaws.com/#{@current_radio.container_name}/#{cleaned_filename(params[:name])}",
+                             headers: {
+                               "Content-Type" => content_type,
+                               "x-amz-acl" => "public-read"
+                             },
+                             body_digest: 'UNSIGNED-PAYLOAD',
+                             expires_in: 10.hours.to_i,
+                            )
+
+    render json: { endpoint: url.to_s }
   end
 
   private
