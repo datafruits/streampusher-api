b4222c44ebb17ec7fb8d19da22ad36d04c3e0ac6
Tony Miller <mcfiredrill@gmail.com>
2019-11-26 18:29:11 +0900

    set portbindings to radio's stored port number
    Former-commit-id: 1d6db0449c37428fbae432c2852346c0c9e03623

--- app/services/radio_booter.rb
+++ app/services/radio_booter.rb
@@ -7,19 +7,30 @@ class RadioBooter
     radio_name = radio.container_name
     redis = Redis.current
 
-    liquidsoap_container = DockerWrapper.find_or_create 'mcfiredrill/liquidsoap:latest',
-      "#{radio_name}_liquidsoap",
-      ["RADIO_NAME=#{radio_name}","RAILS_ENV=#{Rails.env}",
+    image = 'mcfiredrill/liquidsoap:latest'
+    name = "#{radio_name}_liquidsoap"
+    env = ["RADIO_NAME=#{radio_name}","RAILS_ENV=#{Rails.env}",
        "ICECAST_HOST", "icecast",
        "TUNEIN_PARTNER_ID=#{radio.tunein_partner_id}",
        "TUNEIN_PARTNER_KEY=#{radio.tunein_partner_key}",
        "TUNEIN_METADATA_UPDATES_ENABLED=#{radio.tunein_metadata_updates_enabled?}",
        "TUNEIN_STATION_ID=#{radio.tunein_station_id}",
-       "LIQ_SECRET=#{Rails.application.secrets.liq_secret}"],
-      ["#{radio.tracks_directory}:/home/liquidsoap/tracks",
+       "LIQ_SECRET=#{Rails.application.secrets.liq_secret}"]
+    binds = ["#{radio.tracks_directory}:/home/liquidsoap/tracks",
        "#{radio.recordings_directory}:/home/liquidsoap/recordings"]
-    radio.update liquidsoap_container_id: liquidsoap_container.id
-    # radio.update liquidsoap_container_id: liquidsoap_container.id, port_number: liquidsoap_container.port
+    host_ports = {}
+
+    if radio.port_number
+      host_ports['9000'] = radio.port_number
+    end
+
+    liquidsoap_container = DockerWrapper.find_or_create image,
+      name,
+      env,
+      bind,
+      host_ports
+
+    radio.update liquidsoap_container_id: liquidsoap_container.id, port_number: liquidsoap_container.port
     if ::Rails.env.production?
      port = redis.hget "proxy-domain", radio.liquidsoap_proxy_key
      if port.present?
@@ -28,7 +39,6 @@ class RadioBooter
     end
     liquidsoap_container.stop
     liquidsoap_container.start
-    # redis.hset 'proxy-domain', radio.liquidsoap_proxy_key, liquidsoap_container.host_port(9000)
     if ::Rails.env.production?
      port = liquidsoap_container.host_port(9000)
      if port.present?
