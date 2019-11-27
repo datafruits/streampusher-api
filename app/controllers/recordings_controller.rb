3703ee40098072c256540066f5623bd70e8b7b40
Tony Miller <mcfiredrill@gmail.com>
2019-11-27 16:30:53 +0900

    add json response for recordings
    Former-commit-id: 426a41c66a6c34c5c6d21ec4a7083ed997a76904

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -2,6 +2,12 @@ class RecordingsController < ApplicationController
   load_and_authorize_resource
   def index
     @recordings = @current_radio.recordings.unscoped.order("file_created_at DESC")
+    respond_to do |format|
+      format.html
+      format.json {
+        render json: @recordings
+      }
+    end
   end
 
   def show
