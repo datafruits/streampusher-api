b10f61d9b0cb996abadeb99789c8bcb243a510d6
Tony Miller <mcfiredrill@gmail.com>
2020-01-08 00:17:00 +0900

    remove feature specs since they will be re-implemented in ember anyway
    Former-commit-id: d7d03b9dc8dace276a94ab81a4da1573e83579c1

--- spec/features/podcasts_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-feature 'podcasts' do
-  scenario 'non-logged in user can view podcasts'
-end
--- spec/features/radio_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-feature 'radio' do
-  scenario 'admin reboots radio' do
-
-  end
-end
