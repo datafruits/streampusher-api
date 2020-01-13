418858c86b5f3c254c910dcaa764fa7843938fbc
Tony Miller <mcfiredrill@gmail.com>
2020-01-14 01:24:55 +0900

    remove unused services
    Former-commit-id: 3765c5b8cf16d47ce4baa54800540417ba09b4a9

--- app/services/end_free_trial.rb
+++
@@ -1,8 +0,0 @@
-class EndFreeTrial
-  def self.perform subscription
-    subscription.trial_ended!
-    subscription.cancel_stripe_subscription
-    # radio = subscription.radios.first
-    # RadioDisableWorker.perform_later(radio)
-  end
-end
