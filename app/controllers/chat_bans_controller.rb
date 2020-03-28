8e14a6c82477ba2305fd6ac2331dee9b7de90ca9
Tony Miller <mcfiredrill@gmail.com>
2020-03-28 10:37:12 +0900

    switch to using sockets

--- app/controllers/chat_bans_controller.rb
+++ app/controllers/chat_bans_controller.rb
@@ -2,15 +2,25 @@ class ChatBansController < ApplicationController
   def index
     authorize! :admin, :chats
     @connections = Chat.all_connections
+    @bans = Chat.bans
   end
 
   def create
     authorize! :admin, :chats
-    Chat.ban params[:socket_id]
+    Chat.ban chat_ban_params[:socket_id]
+    flash[:notice] = "Banned!"
+    render 'create'
   end
 
   def destroy
     authorize! :admin, :chats
-    Chat.unban params[:socket_id]
+    Chat.unban chat_ban_params[:socket_id]
+    flash[:notice] = "Unbanned!"
+    render 'create'
+  end
+
+  private
+  def chat_ban_params
+    params.require(:chat_ban).permit(:socket_id)
   end
 end
--- app/services/chat.rb
+++ app/services/chat.rb
@@ -1,15 +1,20 @@
 class Chat
   def self.all_connections
-    Redis.current.lrange "datafruits:chat:sockets", 0, -1
+    Redis.current.smembers "datafruits:chat:sockets"
+  end
+
+  def self.bans
+    Redis.current.smembers "datafruits:chat:ips:banned"
   end
 
   def self.ban socket_id
     ip_address = socket_id.split(":").last
-    Redis.current.lpush "datafruits:chat:ips:banned", ip_address
+    Redis.current.sadd "datafruits:chat:ips:banned", ip_address
     Redis.current.publish "datafruits:chat:bans", socket_id
   end
 
   def self.unban socket_id
-
+    ip_address = socket_id.split(":").last
+    Redis.current.srem "datafruits:chat:ips:banned", ip_address
   end
 end
