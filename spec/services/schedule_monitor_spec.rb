ac330d59fcc2692719c2c8b51f258e48a88296d4
Tony Miller <mcfiredrill@gmail.com>
2020-05-20 17:27:56 -0700

    wip fill in spec

--- spec/services/schedule_monitor_spec.rb
+++ spec/services/schedule_monitor_spec.rb
@@ -27,6 +27,16 @@ describe ScheduleMonitor do
     end
   end
 
-  xit "doesn't skip if the current show is not over yet and no_cue_out is set on the current show"
-  xit "skips to the right track if the queue is full"
+  it "doesn't skip if the current show is not over yet and no_cue_out is set on the current show" do
+    playlist.update no_cue_out: true
+    scheduled_show = FactoryBot.create :scheduled_show, playlist: playlist, radio: radio,
+      start_at: Chronic.parse("January 1st 2090 at 10:30 pm"), end_at: Chronic.parse("January 2nd 2090 at 01:30 am")
+    radio.set_current_show_playing scheduled_show.id
+    Timecop.travel Chronic.parse("January 1st 2090 at 11:30 pm") do
+      allow(liquidsoap_socket_class).to receive(:new).with("/tmp/datafruits/liquidsoap.sock").and_return(liquidsoap_socket)
+      expect(liquidsoap_socket).not_to receive(:write).with("icecast.1.skip")
+      ScheduleMonitor.perform radio, Time.now, liquidsoap_socket_class
+    end
+  end
+  xit "skips to the right track if the queue is full" # what was this supposed to mean
 end
