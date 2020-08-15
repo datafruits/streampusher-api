37c5f0bea58f249917b0f98331df45a19c15f9a1
Tony Miller <mcfiredrill@gmail.com>
2020-08-14 23:40:51 -0700

    use token from radio identity

--- app/services/scheduled_show_notification/twitter.rb
+++ app/services/scheduled_show_notification/twitter.rb
@@ -1,10 +1,13 @@
 class ScheduledShowNotification::Twitter
   def self.perform scheduled_show
+    twitter_identity = scheduled_show.radio.social_identities.find_by(provider: 'twitter')
+    token = twitter_identity.token
+    token_secret = twitter_identity.token_secret
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "YOUR_CONSUMER_KEY"
-      config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-      config.access_token        = "YOUR_ACCESS_TOKEN"
-      config.access_token_secret = "YOUR_ACCESS_SECRET"
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = token
+      config.access_token_secret = token_secret
     end
 
     client.update scheduled_show.tweet_text
