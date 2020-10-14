60cab61437753b0c717ec62015d90de9250e0cef
Tony Miller <mcfiredrill@gmail.com>
2020-10-14 10:15:27 +0900

    add avatar url to user_serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :time_zone, :role
+  attributes :id, :username, :email, :time_zone, :role, :avatar_url
   has_many :social_identities
+
+  def avatar_url
+    if object.image.present?
+      object.image.url(:thumb)
+    end
+  end
 end
