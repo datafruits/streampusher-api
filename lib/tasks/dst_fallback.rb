0ae162a47eaf7b6978f25d9bdf20a8830a3f5a0a
Tony Miller <mcfiredrill@gmail.com>
2020-11-04 12:43:25 +0900

    fix rake tasks

--- lib/tasks/dst_fallback.rb
+++
@@ -1,4 +0,0 @@
-desc 'fall back scheduled show times for DST'
-task :dst_fallback do
-  DstFallBackWorker.perform_now
-end
--- lib/tasks/dst_fallforward.rb
+++
@@ -1,4 +0,0 @@
-desc 'fall forward scheduled show times for DST'
-task :dst_fallforward do
-  DstFallForwardWorker.perform_now
-end
