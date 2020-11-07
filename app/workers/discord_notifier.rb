4ba7171c6d159e6070130133f0f728fa217e9716
Tony Miller <mcfiredrill@gmail.com>
2020-11-07 12:38:46 +0900

    use URL in discord_notifier

--- app/workers/discord_notifier.rb
+++ app/workers/discord_notifier.rb
@@ -2,6 +2,6 @@ class DiscordNotifier < ActiveJob::Base
   queue_as :default
 
   def perform message, url = ENV['DISCORD_WEBHOOK_URL']
-    Discord::Notifier.message(message)
+    Discord::Notifier.message(message, url: url)
   end
 end
