d4fd8675c92acbba0c350faa0a5ad65c71f4df96
Tony Miller <mcfiredrill@gmail.com>
2020-11-10 16:50:17 +0900

    remove login logging

--- config/initializers/warden.rb
+++
@@ -1,3 +0,0 @@
-Warden::Manager.before_failure do |env, opts|
-  ActiveSupport::Notifications.instrument 'user.login.failed', user: env["action_dispatch.request.request_parameters"]["user"], path: env["REQUEST_PATH"]
-end
