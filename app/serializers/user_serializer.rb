669d35443457a3013c419f719894574994fbb5f5
Tony Miller <mcfiredrill@gmail.com>
2021-03-17 13:45:17 +0900

    add user avatar to serializer and controller

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,26 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :time_zone, :role, :avatar_url, :style
+  attributes :id, :username, :email, :time_zone, :role, :avatar_url, :style, :avatar, :avatar_filename
   has_many :social_identities
 
   def avatar_url
     if object.image.present?
       object.image.url(:thumb)
+    end
+  end
+
+  def avatar
+    if object.image.present?
+      {
+        basename: object.image_file_name,
+        attachment: "avatars",
+        updated_at: object.image.updated_at
+      }
+    end
+  end
+
+  def avatar_filename
+    if object.image.present?
+      object.image_file_name
     end
   end
 end
