c0173d14e8c44d6418f443f4a31892c504b927e6
palladidrago <38191497+palladidrago@users.noreply.github.com>
2021-03-31 00:51:26 +0300

    Update track.rb

--- app/models/track.rb
+++ app/models/track.rb
@@ -16,7 +16,7 @@ class Track < ActiveRecord::Base
     s3_credentials: { bucket: ENV['S3_BUCKET'],
                       access_key_id: ENV['S3_KEY'],
                       secret_access_key: ENV['S3_SECRET'],
-                      s3_region: ENV['S3_REGION'] },
+                      region: ENV['S3_REGION'] },
     path: ":attachment/:style/:basename.:extension"
 
   validates_attachment_content_type :artwork, content_type: /\Aimage\/.*\Z/
