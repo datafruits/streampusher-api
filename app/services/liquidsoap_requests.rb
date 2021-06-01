459c3562963a0e7e0a99d02d69c21185b59b156c
Tony Miller <mcfiredrill@gmail.com>
2021-06-01 21:21:35 +0900

    show all on air requests

--- app/services/liquidsoap_requests.rb
+++ app/services/liquidsoap_requests.rb
@@ -6,7 +6,7 @@ class LiquidsoapRequests
 
   def self.on_air radio, liquidsoap_socket_class=Liquidsoap::Socket
     liquidsoap_socket = liquidsoap_socket_class.new(radio.liquidsoap_socket_path)
-    liquidsoap_socket.write("request.on_air").gsub(/END/, "").split(" ").first
+    liquidsoap_socket.write("request.on_air").gsub(/END/, "")
   end
 
   def self.metadata radio, rid, liquidsoap_socket_class=Liquidsoap::Socket
