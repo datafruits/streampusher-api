c484ce5a5bd8593ea220cd3f47d61d2a58f379cd
Tony Miller <mcfiredrill@gmail.com>
2021-10-27 19:13:25 -0700

    Revert "turn off uglifier again"
    This reverts commit 8512075690881180093658310b66decabdaea8dd.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   config.public_file_server.enabled = false
 
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
