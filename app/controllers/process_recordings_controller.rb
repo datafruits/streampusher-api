2d1ac9ba87d22ee153fa06f3d3c2cffc1bd5ba1a
Tony Miller <mcfiredrill@gmail.com>
2021-12-18 12:15:02 -0800

    add json API for recordings

--- app/controllers/process_recordings_controller.rb
+++ app/controllers/process_recordings_controller.rb
@@ -2,6 +2,14 @@ class ProcessRecordingsController < ApplicationController
   def create
     ProcessRecordingWorker.perform_later params[:recording_id]
 
-    redirect_to recordings_path
+    respond_to do |format|
+      format.json {
+        @recording = @current_radio.recordings.find(params[:recording_id])
+        render json: @recording
+      }
+      format.html {
+        redirect_to recordings_path
+      }
+    end
   end
 end
