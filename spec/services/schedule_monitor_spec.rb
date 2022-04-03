8b7e09c6d8fb64e25fb96da96c8590a26e4700dc
Tony Miller <mcfiredrill@gmail.com>
2022-04-03 16:28:34 -0700

    fix schedule_monitor_spec

--- spec/services/schedule_monitor_spec.rb
+++ spec/services/schedule_monitor_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 describe ScheduleMonitor do
   before :each do
-    Redis.current = MockRedis.new
+    host = ENV['REDIS_HOST'] || 'redis'
+    port = ENV['REDIS_PORT'] || 6379
+    password = ENV['REDIS_PASSWORD']
+    Redis.new(host: host, port: port, password: password).flushall
   end
   let(:radio){ FactoryBot.create :radio }
   let(:playlist) { FactoryBot.create :playlist, radio: radio }
