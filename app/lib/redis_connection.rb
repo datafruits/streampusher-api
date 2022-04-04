3b8cd0d2ecd53bf8bb83a5705e44034e98a20e4e
Tony Miller <mcfiredrill@gmail.com>
2022-04-03 20:35:07 -0700

    dont set password for redis in test

--- app/lib/redis_connection.rb
+++ app/lib/redis_connection.rb
@@ -3,6 +3,10 @@ module RedisConnection
     host = ENV['REDIS_HOST'] || 'redis'
     port = ENV['REDIS_PORT'] || 6379
     password = ENV['REDIS_PASSWORD']
-    @redis ||= Redis.new(host: host, port: port, password: password)
+    if ::Rails.env.test?
+      @redis ||= Redis.new(host: host, port: port)
+    else
+      @redis ||= Redis.new(host: host, port: port, password: password)
+    end
   end
 end
