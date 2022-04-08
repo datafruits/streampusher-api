403d4918b0b1f5505395234f207918f9cd0e77ec
Tony Miller <mcfiredrill@gmail.com>
2022-04-07 19:36:22 -0700

    add pagination back to recordings_controller

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -2,7 +2,8 @@ class RecordingsController < ApplicationController
   load_and_authorize_resource
   def index
     @recordings = @current_radio.recordings.unscoped.order("file_created_at DESC")
-    render json: @recordings
+    meta = { page: params[:page], total_pages: @recordings.total_pages.to_i }
+    render json: @recordings, meta: meta
   end
 
   def show
