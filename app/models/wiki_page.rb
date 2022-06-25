d98d7561cd10cbb5962bee9d6f08e79d86cf8b1d
Tony Miller <mcfiredrill@gmail.com>
2022-06-25 20:05:58 +0900

    add validations

--- app/models/wiki_page.rb
+++ app/models/wiki_page.rb
@@ -1,3 +1,4 @@
 class WikiPage < ApplicationRecord
   has_many :wiki_page_edits
+  validates_presence_of :title, :body
 end
--- app/models/wiki_page_edit.rb
+++ app/models/wiki_page_edit.rb
@@ -1,4 +1,5 @@
 class WikiPageEdit < ApplicationRecord
   belongs_to :user
   belongs_to :wiki_page
+  validates_presence_of :title, :body
 end
