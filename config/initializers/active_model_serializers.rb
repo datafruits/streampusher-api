eb46380e79a9443dd550fd9de02050223f56f580
Tony Miller <mcfiredrill@gmail.com>
2022-07-17 12:11:19 +0900

    use json api

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -1,4 +1,5 @@
-ActiveModel::Serializer.setup do |config|
-  #config.embed = :ids
-  #config.embed_in_root = true
+ActiveSupport.on_load(:action_controller) do
+  require 'active_model_serializers/register_jsonapi_renderer'
 end
+
+ActiveModelSerializers.config.adapter = :json_api # Default: `:attributes`
