4a9cd45a4083cbf82647c296a9ef3bf79d17e08d
Tony Miller <mcfiredrill@gmail.com>
2022-08-19 00:13:19 -0700

    fix scheduled shows update

--- app/controllers/scheduled_shows_controller.rb
+++ app/controllers/scheduled_shows_controller.rb
@@ -98,7 +98,13 @@ class ScheduledShowsController < ApplicationController
   def update
     @scheduled_show = @current_radio.scheduled_shows.friendly.find(params[:id])
     authorize! :update, @scheduled_show
-    @scheduled_show.attributes = create_params
+    if create_params[:image].present?
+      image = Paperclip.io_adapters.for(create_params[:image])
+      image.original_filename = create_params.delete(:image_filename)
+      @scheduled_show.attributes = create_params.except(:image_filename).merge({image: image})
+    else
+      @scheduled_show.attributes =  create_params.except(:image_filename).except(:image)
+    end
     if @scheduled_show.save
       ActiveSupport::Notifications.instrument 'scheduled_show.updated', current_user: current_user.email, radio: @current_radio.name, show: @scheduled_show.title, params: create_params
       flash[:notice] = "Updated scheduled show!"
