5c20352427596d97d67f91ce2d5ca0a311223054
Tony Miller <mcfiredrill@gmail.com>
2022-09-19 21:01:52 -0700

    add deleted_at to wiki_pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_13_173636) do
+ActiveRecord::Schema.define(version: 2022_09_20_035546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -412,6 +412,7 @@ ActiveRecord::Schema.define(version: 2022_09_13_173636) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.datetime "deleted_at"
     t.index ["slug"], name: "index_wiki_pages_on_slug", unique: true
   end
 
