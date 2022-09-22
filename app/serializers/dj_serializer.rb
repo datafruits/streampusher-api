485463422eb52b4e398d1efc7553ba4129b0c641
Tony Miller <mcfiredrill@gmail.com>
2022-09-21 20:44:31 -0700

    add fruits affinity scores to user serializers

--- app/serializers/dj_serializer.rb
+++ app/serializers/dj_serializer.rb
@@ -1,6 +1,6 @@
 class DjSerializer < ActiveModel::Serializer
   attributes :id, :username, :image_url, :bio, :image, :style,
-    :image_thumb_url, :image_medium_url, :profile_publish, :pronouns, :role
+    :image_thumb_url, :image_medium_url, :profile_publish, :pronouns, :role, :fruits_affinity
 
   def image_url
     if object.image.present?
@@ -18,5 +18,9 @@ class DjSerializer < ActiveModel::Serializer
     if object.image.present?
       object.image.url(:medium)
     end
+  end
+
+  def fruits_affinity
+    Redis.current.hgetall "datafruits:user_fruit_count:#{object.username}"
   end
 end
