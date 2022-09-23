e51686b6f99973b2bb4e71880f094cbdfedcd0b5
Tony Miller <mcfiredrill@gmail.com>
2022-09-22 19:31:18 -0700

    include modules in format.xml block

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,7 +1,5 @@
 class PodcastsController < ApplicationController
   include ActionController::MimeResponds
-  include ActionView::Layouts
-  include ActionController::Rendering
 
   load_and_authorize_resource except: [:show]
   before_action :current_radio_required, only: [:show]
@@ -40,7 +38,11 @@ class PodcastsController < ApplicationController
   def show
     @podcast = @current_radio.podcasts.find_by_name(params[:id])
     respond_to do |format|
-      format.xml { render 'show', layout: false }
+      format.xml {
+        include ActionView::Layouts
+        include ActionController::Rendering
+        render 'show', layout: false
+      }
       format.json {
         response.headers["Access-Control-Allow-Origin"] = "*" # This is a public API, maybe I should namespace it later
         render json: @podcast, include: 'tracks', meta: { total_pages: @podcast.playlist.playlist_tracks.page.total_pages.to_i, page: params[:page] }
