0425088f6adce201700b561d19288b64017ef86b
Tony Miller <mcfiredrill@gmail.com>
2022-12-05 16:00:17 -0800

    fix bug in SupportersMonthlyFruitTicketsWorker

--- app/workers/supporters_monthly_fruit_tickets_worker.rb
+++ app/workers/supporters_monthly_fruit_tickets_worker.rb
@@ -2,7 +2,7 @@ class SupportersMonthlyFruitTicketsWorker < ActiveJob::Base
   queue_as :default
 
   def perform radio_id
-    Radio.find(radio_id).users.where("role like (?)", "%supporter%") do |user|
+    Radio.find(radio_id).users.where("role like (?)", "%supporter%").find_each do |user|
       puts "giving tickets to #{user.username}"
       fruit_ticket_transaction = FruitTicketTransaction.new to_user: user, amount: 1000, transaction_type: :supporter_membership
       fruit_ticket_transaction.transact_and_save!
