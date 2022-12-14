cefbb412b5ba88d161ffe909dd25d4f374afbb64
Tony Miller <mcfiredrill@gmail.com>
2022-12-13 22:03:12 -0800

    fix polymorphic columns

--- db/migrate/20221214053829_create_posts.rb
+++ db/migrate/20221214053829_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.references :user, index: true, null: false
-      t.references :source, index: true, null: false
+      t.references :postable, polymorphic: true, index: true, null: false
       t.text :body, null: false
 
       t.timestamps
