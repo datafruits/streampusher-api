c999bbf30c163aa0bd0b8bbf160388df4a237c46
Tony Miller <mcfiredrill@gmail.com>
2022-12-20 21:54:39 -0800

    award users a badge for clicking the fruit buttons

---
+++ app/workers/badge_awards_worker.rb
@@ -0,0 +1,20 @@
+class BadgeAwardsWorker < ActiveJob::Base
+  FRUITS = ["strawberry", "lemon", "orange", "cabbage", "banana", "watermel"]
+  AWARD_THRESHOLD = 50_000
+
+  include RedisConnection
+  queue :default
+
+  def perform radio_id
+    radio = Radio.find radio_id
+    radio.users.find_each do |user|
+      FRUITS.each do |fruit|
+        count = redis.hget "datafruits:user_fruit_count:#{user.username}", fruit
+        if count >= AWARD_THRESHOLD & !user.roles.include?(fruit)
+          user.roles << fruit
+          user.save
+        end
+      end
+    end
+  end
+end
