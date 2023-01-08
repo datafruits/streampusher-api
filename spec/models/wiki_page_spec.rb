ae7d8019b27089e06b87e77e0966186e703b8fad
Tony Miller <mcfiredrill@gmail.com>
2023-01-08 15:40:37 -0800

    make sure wiki page slugs update on save
    add a callback to set the slug to nil, this ensures a new slug is
    generated

--- spec/models/wiki_page_spec.rb
+++ spec/models/wiki_page_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe WikiPage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'updates the slug when title changes' do
+    user = FactoryBot.create :user
+    wiki_page = WikiPage.new
+    params = { title: "doo dinglius", body: "doobah" }
+    wiki_page.save_new_edit! params, user.id
+    expect(wiki_page.persisted?).to eq true
+    expect(wiki_page.title).to eq "doo dinglius"
+    expect(wiki_page.slug).to eq "doo-dinglius"
+
+    params[:title] = "doo dinglius 2??"
+    wiki_page.save_new_edit! params, user.id
+    wiki_page.reload
+    expect(wiki_page.title).to eq "doo dinglius 2??"
+    expect(wiki_page.slug).to eq "doo-dinglius-2"
+  end
 end
