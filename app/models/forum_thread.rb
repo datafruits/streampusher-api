77b0ee1b3d67104c82c129c69828634e5ab1898a
Tony Miller <mcfiredrill@gmail.com>
2023-01-14 09:46:26 -0800

    save posts with thread

--- app/models/forum_thread.rb
+++ app/models/forum_thread.rb
@@ -1,3 +1,11 @@
 class ForumThread < ApplicationRecord
+  validates :title, presence: true
   has_many :posts, as: :postable
+
+  def save_new_thread! user, title, body
+    self.title = title
+    post = self.posts.new body: body
+    post.user = user
+    self.save! && post.save!
+  end
 end
--- spec/models/forum_thread_spec.rb
+++ spec/models/forum_thread_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ForumThread, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'saves new thread and post' do
+    user = FactoryBot.create :user
+    forum_thread = ForumThread.new
+    forum_thread.save_new_thread! user, "test", "hey blahblahblahbalbhah"
+    expect(forum_thread.persisted?).to eq true
+    expect(forum_thread.title).to eq "test"
+    expect(forum_thread.posts.count).to eq 1
+    expect(forum_thread.posts.first.body).to eq "hey blahblahblahbalbhah"
+    expect(forum_thread.posts.first.user).to eq user
+  end
 end
