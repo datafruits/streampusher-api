3d725b33132cfc9f930ded567457e17746eca595
Tony Miller <mcfiredrill@gmail.com>
2023-01-24 12:15:10 -0800

    refactor regexes to application_record shared method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.friendly_find id
+    return self.friendly.find id.downcase.gsub(/[^0-9a-z\-\s]/i, '').strip.gsub("\s", "-")
+  end
 end
