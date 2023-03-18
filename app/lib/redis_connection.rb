6a93840853583effe68259e2f6bfd4a2dde20f1a
Sam Raymer <sam@substantial.com>
2023-03-18 00:46:18 +0000

    test env uses redis password

--- app/lib/redis_connection.rb
+++ app/lib/redis_connection.rb
@@ -3,10 +3,8 @@ module RedisConnection
     host = ENV['REDIS_HOST'] || 'redis'
     port = ENV['REDIS_PORT'] || 6379
     password = ENV['REDIS_PASSWORD']
-    if ::Rails.env.test?
-      @redis ||= Redis.new(host: host, port: port)
-    else
-      @redis ||= Redis.new(host: host, port: port, password: password)
-    end
+    @redis ||= password.nil? ?
+      Redis.new(host: host, port: port) :
+      Redis.new(host: host, port: port, password: password)
   end
 end
