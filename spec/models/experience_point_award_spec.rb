71c593f6c96b3686a6c01dbd05c61e540d73a7c8
Tony Miller <mcfiredrill@gmail.com>
2023-04-01 14:00:05 -0700

    spec

--- spec/models/experience_point_award_spec.rb
+++ spec/models/experience_point_award_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.describe ExperiencePointAward, type: :model do
   let(:user){ User.create username: "mcfiredrill", time_zone: "Tokyo", role: "admin owner" }
   describe "leveling up" do
-    xit "raises level by 1 if xp threshold reached"
+    it "raises level by 1 if xp threshold reached" do
+      user = FactoryBot.create :user
+
+      ExperiencePointAward.create user: user, amount: 1000, award_type: :music_enjoyer
+
+      expect(user.level).to eq 2
+    end
   end
 end
