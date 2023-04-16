8d78a497f678685d3acf5fde3e7e8b63e41d0066
Tony Miller <mcfiredrill@gmail.com>
2023-04-16 15:01:22 -0700

    pull payout_fruit_ticket_track_plays into a service and test that instead
    This way we can avoid handling the vcr stuff and just test the pure
    logic inside the service.

--- spec/workers/payout_fruit_ticket_track_plays_worker_spec.rb
+++ spec/workers/payout_fruit_ticket_track_plays_worker_spec.rb
@@ -1,12 +1,8 @@
 require 'rails_helper'
-require 'sidekiq/testing'
 
 describe PayoutFruitTicketTrackPlaysWorker do
   include RedisConnection
 
-  before do
-    Sidekiq::Testing.inline!
-  end
   before :each do
     host = ENV['REDIS_HOST'] || 'redis'
     port = ENV['REDIS_PORT'] || 6379
@@ -19,7 +15,7 @@ describe PayoutFruitTicketTrackPlaysWorker do
     scheduled_show = FactoryBot.create :scheduled_show, dj: user, start_at: 2.days.from_now, end_at: 3.days.from_now, playlist: playlist, radio: radio
     track = FactoryBot.create :track, scheduled_show: scheduled_show, radio: radio
     redis.hset("datafruits:track_plays", track.id, 3)
-    PayoutFruitTicketTrackPlaysWorker.perform_now
+    PayoutFruitTicketTrackPlays.new.perform
     user.reload
     expect(user.fruit_ticket_balance).to eq 3
 
