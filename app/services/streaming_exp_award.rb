17bc97d87a0669bcad4cb579314f09609e67a4db
Tony Miller <mcfiredrill@gmail.com>
2023-04-16 10:58:57 -0700

    use create! in streaming_exp_award

--- app/services/streaming_exp_award.rb
+++ app/services/streaming_exp_award.rb
@@ -1,7 +1,7 @@
 class StreamingExpAward
   def self.perform track
     if track.uploaded_by && track.length
-      ExperiencePointAward.create user: track.uploaded_by, source_id: track.id, award_type: "streaming_streamer", amount: track.length / 30
+      ExperiencePointAward.create! user: track.uploaded_by, source_id: track.id, award_type: "streaming_streamer", amount: track.length / 30
     end
   end
 end
