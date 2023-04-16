4581e99859a9945d4d927f3f69d095f752a9fa5b
Tony Miller <mcfiredrill@gmail.com>
2023-04-16 10:53:43 -0700

    try to fix payout_fruit_ticket_track_plays_worker_spec
    Trying to fix this, keeps getting an error:
    
      1) PayoutFruitTicketTrackPlaysWorker pays out royalties for track plays
         Failure/Error: track = FactoryBot.create :track, radio: radio
    
         Aws::S3::Errors::NoSuchKey:
           The specified key does not exist.
         # ./vendor/bundle/ruby/2.7.0/gems/aws-sdk-core-3.160.0/lib/seahorse/client/plugins/raise_respo
    nse_errors.rb:17:in `call'
         # ./vendor/bundle/ruby/2.7.0/gems/aws-sdk-s3-1.114.0/lib/aws-sdk-s3/plugins/skip_whole_multipa
    rt_get_checksums.rb:18:in `call'
         # ./vendor/bundle/ruby/2.7.0/gems/aws-sdk-s3-1.114.0/lib/aws-sdk-s3/plugins/sse_cpk.rb:24:in `
    call'
         # ./vendor/bundle/ruby/2.7.0/gems/aws-sdk-s3-1.114.0/lib/aws-sdk-s3/plugins/dualstack.rb:27:in
     `call'
         # ./vendor/bundle/ruby/2.7.0/gems/aws-sdk-s3-1.114.0/lib/aws-sdk-s3/plugins/accelerate.rb:56:i
    n `call'
         # ./vendor/bundle/ruby/2.7.0/gems/aws-sdk-core-3.160.0/lib/aws-sdk-core/plugins/checksum_algor
    
    I believe it's trying to upload and image for the associated scheduled
    show, but I'm not sure why it's saying 'The specified key does not
    exist'.

--- spec/workers/payout_fruit_ticket_track_plays_worker_spec.rb
+++ spec/workers/payout_fruit_ticket_track_plays_worker_spec.rb
@@ -13,17 +13,19 @@ describe PayoutFruitTicketTrackPlaysWorker do
     redis.flushall
   end
   it "pays out royalties for track plays" do
-    radio = FactoryBot.create :radio
-    user = FactoryBot.create :user, fruit_ticket_balance: 0
-    playlist = FactoryBot.create :playlist, radio: radio
-    scheduled_show = FactoryBot.create :scheduled_show, dj: user, start_at: 2.days.from_now, end_at: 3.days.from_now, playlist: playlist, radio: radio
-    track = FactoryBot.create :track, scheduled_show: scheduled_show, radio: radio
-    redis.hset("datafruits:track_plays", track.id, 3)
-    PayoutFruitTicketTrackPlaysWorker.perform_now
-    user.reload
-    expect(user.fruit_ticket_balance).to eq 3
+    VCR.use_cassette(RSpec.current_example.metadata[:full_description].to_s) do
+      radio = FactoryBot.create :radio
+      user = FactoryBot.create :user, fruit_ticket_balance: 0
+      playlist = FactoryBot.create :playlist, radio: radio
+      scheduled_show = FactoryBot.create :scheduled_show, dj: user, start_at: 2.days.from_now, end_at: 3.days.from_now, playlist: playlist, radio: radio
+      track = FactoryBot.create :track, radio: radio
+      redis.hset("datafruits:track_plays", track.id, 3)
+      PayoutFruitTicketTrackPlaysWorker.perform_now
+      user.reload
+      expect(user.fruit_ticket_balance).to eq 3
 
-    fruit_ticket_transaction = FruitTicketTransaction.last
-    expect(fruit_ticket_transaction.source_id).to eq track.id
+      fruit_ticket_transaction = FruitTicketTransaction.last
+      expect(fruit_ticket_transaction.source_id).to eq track.id
+    end
   end
 end
