037952cb7fde15618b4fde1ca35dd94d365c065b
Tony Miller <mcfiredrill@gmail.com>
2023-04-17 16:04:23 -0700

    make sure streaming_exp_award is not given twice
    Maybe some db constraints could help as well.

--- app/services/streaming_exp_award.rb
+++ app/services/streaming_exp_award.rb
@@ -1,7 +1,9 @@
 class StreamingExpAward
   def self.perform track
     if track.uploaded_by && track.length
-      ExperiencePointAward.create! user: track.uploaded_by, source_id: track.id, award_type: "streaming_streamer", amount: track.length / 30
+      unless ExperiencePointAward.where(source_id: track.id).exists?
+        ExperiencePointAward.create! user: track.uploaded_by, source_id: track.id, award_type: "streaming_streamer", amount: track.length / 30
+      end
     end
   end
 end
