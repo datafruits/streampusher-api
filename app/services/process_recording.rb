3c7d8bf59f3e89c4016c95a23250f7a523a62956
Tony Miller <mcfiredrill@gmail.com>
2023-04-16 20:52:14 -0700

    wait 30 sec to queue exp points award worker

--- app/services/process_recording.rb
+++ app/services/process_recording.rb
@@ -21,7 +21,7 @@ class ProcessRecording
       username = basename.split("datafruits-").last.split("-").first
       user = User.find_by(username: username)
       track = radio.tracks.create! audio_file_name: audio_file_name, uploaded_by: user
-      StreamingExpAwardWorker.perform_later track.id
+      StreamingExpAwardWorker.perform_in 30.seconds, track.id
 
       # if scheduled_show.present?
       #   # grab scheduled show's info to use metadata and artwork
