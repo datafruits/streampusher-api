823bc29df7502585fbe457cd00982c56cc38c54c
Tony Miller <mcfiredrill@gmail.com>
2023-04-19 10:10:06 -0700

    notif messages

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -11,9 +11,30 @@ class Notification < ApplicatinoRecord
     :banana_badge_award,
     :cabbage_badge_award,
     :watermelon_badge_award,
-    :level_up,
+    :level_up_award,
     :experience_point_award
   ]
+
+  def message
+    case self.notification_type
+    when :strawberry_badge_award
+      "#{self.user.username} got the strawbur badge!"
+    when :lemon_badge_award
+      "#{self.user.username} got the lemoner badge!"
+    when :orange_badge_award
+      "#{self.user.username} got the orangey badge!"
+    when :banana_badge_award
+      "#{self.user.username} got the banaynay badge!"
+    when :cabbage_badge_award
+      "#{self.user.username} got the evil cabbage badge!"
+    when :watermelon_badge_award
+      "#{self.user.username} got the watermel badge!"
+    when :level_up
+      "#{self.user.username} reached level #{self.user.level}!"
+    when :experience_point_award
+      "You got #{self.source.amount} #{self.source.award_type} points!"
+    end
+  end
 
   private
   def send_notification
