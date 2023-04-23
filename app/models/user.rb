57b40945740cb096cbab2d1b5293c573fa251f85
Tony Miller <mcfiredrill@gmail.com>
2023-04-23 16:06:25 -0700

    create notification on level up

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ActiveRecord::Base
   has_many :track_favorites
   has_many :fruit_summons
   has_many :posts
+  has_many :notifications
 
   has_attached_file :image, styles: { :thumb => "150x150#", :medium => "250x250#" },
     path: ":attachment/:style/:basename.:extension"
