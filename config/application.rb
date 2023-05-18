e60f86b5b5a63bd34eae8b93ff2dc308c790387e
Tony Miller <mcfiredrill@gmail.com>
2023-05-17 20:56:10 -0700

    use File.exist

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module StreamPusher
       env_file = File.join(Rails.root, 'config', 'application.yml')
       YAML.load(File.open(env_file))[Rails.env].each do |key, value|
         ENV[key.to_s] = value
-      end if File.exists?(env_file)
+      end if File.exist?(env_file)
     end
 
     config.middleware.use ActionDispatch::Cookies
