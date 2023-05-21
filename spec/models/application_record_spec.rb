59775ee3e64cebc47c69c214ec4e0ffc3b50583f
Tony Miller <mcfiredrill@gmail.com>
2023-05-21 16:19:52 -0700

    fix spec
    not sure why that was in application_record_spec

--- spec/models/application_record_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ApplicationRecord, type: :model do
-  describe '.friendly_find' do
-    thread = ForumThread.create! title: "can glorp do a glorp ?? ! "
-    binding.pry
-    expect(ForumThread.friendly_find("can a glorp do a glorp ?? ! ")).to eq thread
-  end
-end
