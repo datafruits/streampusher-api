d7b4d7a405dd52a0ea898d4540a7ed79b1c52066
Tony Miller <mcfiredrill@gmail.com>
2023-05-27 14:28:16 -0700

    fix ScheduledShowExpAwardWorker

--- app/workers/scheduled_show_exp_award_worker.rb
+++ app/workers/scheduled_show_exp_award_worker.rb
@@ -2,7 +2,7 @@ class ScheduledShowExpAwardWorker < ActiveJob::Base
   queue_as :default
 
   def perform scheduled_show_id
-    scheduled_show = ScheduledShow.find scheduled_show
+    scheduled_show = ScheduledShow.find scheduled_show_id
     ScheduledShowExpAward.perform scheduled_show
   end
 end
