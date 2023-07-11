b09190442ee4c81d1aa024c128c4f6c6d0172b49
Sam Raymer <sam@substantial.com>
2023-07-11 00:46:13 +0000

    fruit summon hack around rails 7 transaction rollback

--- app/services/fruit_summon_transaction.rb
+++ app/services/fruit_summon_transaction.rb
@@ -10,7 +10,9 @@ class FruitSummonTransaction
         fruit_summon = user.fruit_summons.new fruit_ticket_transaction: fruit_ticket_transaction, fruit_summon_entity: entity, user: user
         fruit_summon.save!
 
-        return fruit_summon
+        # we had a 'return' before but that rolls back the transaction for some reason
+        # see https://stackoverflow.com/a/73621642
+        fruit_summon
       rescue
         puts "fruit summon failed, user #{user.name} entity #{entity.name}"
       end
