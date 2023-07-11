c83470543efcc1c423847ef0d6f4ec3468a32932
Sam Raymer <sam@substantial.com>
2023-07-11 00:12:12 +0000

    fruit summon error handling

--- app/services/fruit_summon_transaction.rb
+++ app/services/fruit_summon_transaction.rb
@@ -4,11 +4,16 @@ class FruitSummonTransaction
     raise "invalid fruit summon" unless entity
 
     ActiveRecord::Base.transaction do
-      fruit_ticket_transaction = FruitTicketTransaction.new transaction_type: :fruit_summon, from_user: user, source_id: entity.id
-      fruit_ticket_transaction.transact_and_save!
-      fruit_summon = user.fruit_summons.new fruit_ticket_transaction: fruit_ticket_transaction, fruit_summon_entity: entity
-      fruit_summon.save!
-      return fruit_summon
+      begin
+        fruit_ticket_transaction = FruitTicketTransaction.new transaction_type: :fruit_summon, from_user: user, source_id: entity.id
+        fruit_ticket_transaction.transact_and_save!
+        fruit_summon = user.fruit_summons.new fruit_ticket_transaction: fruit_ticket_transaction, fruit_summon_entity: entity, user: user
+        fruit_summon.save!
+
+        return fruit_summon
+      rescue
+        puts "fruit summon failed, user #{user.name} entity #{entity.name}"
+      end
     end
   end
 end
