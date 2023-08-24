ccda83ac1e554b5c2903f8f36a737c5c89aa7ceb
Tony Miller <mcfiredrill@gmail.com>
2023-08-24 09:19:37 -0700

    add shrimpo serializer

--- app/serializers/shrimpo_serializer.rb
+++ app/serializers/shrimpo_serializer.rb
@@ -1,3 +1,6 @@
 class ShrimpoSerializer < ActiveModel::Serializer
-  attributes :title, :rule_pack, :start_at, :end_at
+  attributes :title, :rule_pack, :start_at, :end_at, :zip_file_url
+
+  def zip_file_url
+  end
 end
