334f05ed2ead592799d986c6dcbd4ee6d983732b
Tony Miller <mcfiredrill@gmail.com>
2023-09-07 12:15:39 -0700

    add status to show_series_serializer

--- app/serializers/show_series_serializer.rb
+++ app/serializers/show_series_serializer.rb
@@ -1,6 +1,7 @@
 class ShowSeriesSerializer < ActiveModel::Serializer
   attributes :id, :recurring_interval, :recurring_cadence, :recurring_weekday, :title, :description,
-    :image_url, :thumb_image_url, :image, :image_filename, :slug, :start_time, :end_time, :start_date, :end_date
+    :image_url, :thumb_image_url, :image, :image_filename, :slug, :start_time, :end_time, :start_date, :end_date,
+    :status
   has_many :labels, embed: :ids, key: :labels, embed_in_root: true, each_serializer: LabelSerializer
   has_many :users, embed: :ids, key: :users, embed_in_root: true, each_serializer: DjSerializer
 
