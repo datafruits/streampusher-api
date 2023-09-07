7f6d953754b054646b64efdaab6ac391137b332e
Tony Miller <mcfiredrill@gmail.com>
2023-09-07 11:38:47 -0700

    fix saving one off shows sort of

--- app/controllers/api/my_shows_controller.rb
+++ app/controllers/api/my_shows_controller.rb
@@ -17,23 +17,26 @@ class Api::MyShowsController < ApplicationController
     authorize! :create, ShowSeries
     if my_show_params[:recurring_interval] === "not_recurring"
       guest_series = ShowSeries.find_by(title: "GuestFruits")
-      episode = guest_series.episodes.new my_show_params.except(:recurring_interval, :start_date, :end_date, :start_time, :end_time)
+      episode = guest_series.episodes.new my_show_params.except(:recurring_interval, :recurring_cadence, :recurring_weekday, :start_date, :end_date, :start_time, :end_time)
       episode.start_at = my_show_params[:start_time]
       episode.end_at = my_show_params[:end_time]
-      # TODO image??
+      episode.playlist = guest_series.radio.default_playlist
+      episode.dj_id = users_params[:user_ids].first
+      episode.radio_id = guest_series.radio_id
+      # TODO add hosts
+      # if users_params.has_key? :user_ids
+      #   users_params[:user_ids].each do |user_id|
+      #     show_series.show_series_hosts.build user_id: user_id
+      #   end
+      # end
+      # TODO add labels
       if episode.save
         render json: guest_series
       else
-        render json: { errors: guest_series.errors }, status: 422
+        render json: { errors: episode.errors }, status: 422
       end
     else
-      if my_show_params[:image].present?
-        image = Paperclip.io_adapters.for(my_show_params[:image])
-        image.original_filename = my_show_params.delete(:image_filename)
-        show_series = ShowSeries.new my_show_params.except(:image_filename).merge({image: image})
-      else
-        show_series = ShowSeries.new my_show_params.except(:image_filename).except(:image)
-      end
+      show_series = ShowSeries.new my_show_params
       if users_params.has_key? :user_ids
         users_params[:user_ids].each do |user_id|
           show_series.show_series_hosts.build user_id: user_id
@@ -55,13 +58,7 @@ class Api::MyShowsController < ApplicationController
   def update
     show_series = ShowSeries.friendly.find params[:id]
     authorize! :update, show_series
-    if my_show_params[:image].present?
-      image = Paperclip.io_adapters.for(my_show_params[:image])
-      image.original_filename = my_show_params.delete(:image_filename)
-      show_series = ShowSeries.new my_show_params.except(:image_filename).merge({image: image})
-    else
-      show_series = ShowSeries.new my_show_params.except(:image_filename).except(:image)
-    end
+    show_series = ShowSeries.new my_show_params
     if show_series.save
       render json: show_series
     else
@@ -71,7 +68,7 @@ class Api::MyShowsController < ApplicationController
 
   private
   def my_show_params
-    ActiveModelSerializers::Deserialization.jsonapi_parse(params, only: [
+    p = ActiveModelSerializers::Deserialization.jsonapi_parse(params, only: [
       :title,
       :start_date,
       :end_date,
@@ -83,6 +80,13 @@ class Api::MyShowsController < ApplicationController
       :recurring_weekday,
       :start, :end
     ])
+    if p[:image].present?
+      image = Paperclip.io_adapters.for(p[:image])
+      image.original_filename = p.delete(:image_filename)
+      return p.except(:image_filename).merge({image: image})
+    else
+      return p.except(:image_filename).except(:image)
+    end
   end
 
   def labels_params
