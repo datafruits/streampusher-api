c7a5f1aeaeae3d4f63745707ff08114881782465
Tony Miller <mcfiredrill@gmail.com>
2023-10-05 09:08:07 -0700

    check if there are any tracks in playlist for prerecord_track_id

--- app/models/scheduled_show.rb
+++ app/models/scheduled_show.rb
@@ -62,7 +62,7 @@ class ScheduledShow < ActiveRecord::Base
   attr_accessor :prerecord_track_id
 
   def prerecord_track_id
-    self.playlist.tracks.first.id if self.playlist.present?
+    self.playlist.tracks.first.id if self.playlist.present? && self.playlist.tracks.any?
   end
 
   def prerecord_track_id= track_id
