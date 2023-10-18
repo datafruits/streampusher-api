fff35df797a30c27757af9348eb947d010fe6663
Tony Miller <mcfiredrill@gmail.com>
2023-10-17 20:27:38 -0700

    change episodes ordering for my-shows

--- app/controllers/api/show_series/episodes_controller.rb
+++ app/controllers/api/show_series/episodes_controller.rb
@@ -1,10 +1,13 @@
 class Api::ShowSeries::EpisodesController < ApplicationController
   def index
     episodes = ::ShowSeries.friendly.find(params[:show_series_id]).episodes #.page(params[:page])
+    # FIXME change the query param naming
+    # past episodes, could be published or unpublished
     if params[:status] === "archive_published"
-      episodes = episodes.where(status: "archive_published").order("start_at DESC")
+      episodes = episodes.where("start_at < ?", Time.now).order("start_at DESC")
+    # future episodes start at ASC shouldn't be published
     elsif params[:status] === "archive_unpublished"
-      episodes = episodes.where(status: "archive_unpublished").order("start_at ASC")
+      episodes = episodes.where("start_at > ?", Time.now).order("start_at ASC")
     end
     episodes = episodes.page(params[:page])
     render json: episodes
