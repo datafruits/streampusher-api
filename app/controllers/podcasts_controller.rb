33badb72a24c2193683ed143ee7d37073e6c8ccc
Tony Miller <mcfiredrill@gmail.com>
2023-10-27 19:55:37 -0700

    fix archives search

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -43,10 +43,6 @@ class PodcastsController < ApplicationController
       format.xml {
         render 'show', layout: false
       }
-      format.json {
-        response.headers["Access-Control-Allow-Origin"] = "*" # This is a public API, maybe I should namespace it later
-        render json: @podcast, include: 'tracks', meta: { total_pages: @podcast.playlist.playlist_tracks.page.total_pages.to_i, page: params[:page] }, serializer: PodcastSerializer, layout: false
-      }
     end
   end
 
