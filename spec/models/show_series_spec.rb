ab5cad6abd94f04be3a8edf6e85438bcee093353
Tony Miller <mcfiredrill@gmail.com>
2023-11-09 09:14:34 -0800

    stop using pst monkeypatch in tests

--- spec/models/show_series_spec.rb
+++ spec/models/show_series_spec.rb
@@ -75,23 +75,33 @@ RSpec.describe ShowSeries, type: :model do
     end
 
     it "handles crossing the DST boundry" do
-      Timecop.travel Time.zone.parse("2015-11-13") do
+      Timecop.travel Time.zone.parse("2015-06-13") do
         start_time = Chronic.parse("beginning of the month at 3pm")
         show_series = ShowSeries.new title: "monthly jammer jam", description: "wow", recurring_interval: "month", recurring_weekday: 'Sunday', recurring_cadence: 'First', start_time: start_time, end_time: start_time + 1.hours, start_date: Date.today.beginning_of_month + 1.month, radio: @radio, time_zone: "Pacific Time (US & Canada)"
         show_series.users << @dj
         show_series.save!
+        puts Time.now.in_time_zone("US/Pacific")
 
         # should be the same time always in pst/pdt
-        expect(show_series.episodes.pluck(:start_at).map{|m| m.pst.hour }.uniq.count).to eq 1
+        expect(show_series.episodes.pluck(:start_at).map{|m| m.in_time_zone(show_series.time_zone).hour }.uniq.count).to eq 1
         # should be two different times in UTC
         expect(show_series.episodes.pluck(:start_at).map{|m| m.hour }.uniq.count).to eq 2
-        expect(show_series.episodes.pluck(:start_at).map{|m| m.pst.hour }.uniq.first).to eq 7
-        expect(show_series.episodes.pluck(:end_at).map{|m| m.pst.hour }.uniq.first).to eq 8
+        # check pst time is correct
+        expect(show_series.episodes.pluck(:start_at).map{|m| m.in_time_zone(show_series.time_zone).hour }.uniq.first).to eq 9
+        expect(show_series.episodes.pluck(:end_at).map{|m| m.in_time_zone(show_series.time_zone).hour }.uniq.first).to eq 10
 
         pre_dst_start_hour = show_series.episodes.first.start_at.in_time_zone(show_series.time_zone).hour
         dst_episode = show_series.episodes.where("start_at >= ?", show_series.start_date + 6.months).first
         post_dst_start_hour = dst_episode.start_at.in_time_zone(show_series.time_zone).hour
         expect(post_dst_start_hour).to eq(pre_dst_start_hour)
+
+        Timecop.travel 6.months.since do
+          puts Time.now.in_time_zone("US/Pacific")
+          show_series.episodes.future.destroy_all
+          show_series.save_episodes
+          expect(show_series.episodes.pluck(:start_at).map{|m| m.in_time_zone(show_series.time_zone).hour }.uniq.first).to eq 9
+          expect(show_series.episodes.pluck(:end_at).map{|m| m.in_time_zone(show_series.time_zone).hour }.uniq.first).to eq 10
+        end
       end
     end
 
