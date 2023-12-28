7c9aa258e542f00a5793d2bcb89004febb9f100a
Tony Miller <mcfiredrill@gmail.com>
2023-12-28 13:05:24 -0800

    add avatar url to dj_serializer

--- app/serializers/fast/dj_serializer.rb
+++ app/serializers/fast/dj_serializer.rb
@@ -4,7 +4,13 @@ class Fast::DjSerializer
   attributes :id, :username, :image_url, :bio, :image, :style, :email,
     :image_thumb_url, :image_medium_url, :profile_publish, :pronouns, :role,
     :homepage, :fruit_ticket_balance, :created_at, :last_sign_in_at,
-    :level, :experience_points, :xp_needed_for_next_level
+    :level, :experience_points, :xp_needed_for_next_level, :avatar_url
+
+  attribute :avatar_url do |object|
+    if object.image.present?
+      CGI.unescape(object.image.url(:thumb))
+    end
+  end
 
   attribute :image_url do |object|
     if object.image.present?
