b91bae1966ba35d93af0f1f89d1120461b137648
Tony Miller <mcfiredrill@gmail.com>
2023-12-29 12:56:27 -0800

    store track/show url in metadata so we can link to it from the now playing bar

--- app/services/metadata_publisher.rb
+++ app/services/metadata_publisher.rb
@@ -2,5 +2,21 @@ class MetadataPublisher
   def self.perform radio, metadata
     StreamPusher.redis.set "#{radio}:metadata", metadata
     StreamPusher.redis.publish "#{radio}:metadata", metadata
+    track = Track.where(title: metadata)
+    if track.present?
+      if track.scheduled_show
+        url = "/shows/#{show_series.slug}/episodes/#{track.scheduled_show.slug}"
+        StreamPusher.redis.set "#{radio}:metadata:url", url
+        StreamPusher.redis.publish "#{radio}:metadata:url", url
+      end
+    elsif metadata.starts_with? "LIVE"
+      show = radio.current_scheduled_show
+      if show.present?
+        show_series = show.show_series
+        url = "/shows/#{show_series.slug}/episodes/#{show.slug}"
+        StreamPusher.redis.set "#{radio}:metadata:url", url
+        StreamPusher.redis.publish "#{radio}:metadata:url", url
+      end
+    end
   end
 end
