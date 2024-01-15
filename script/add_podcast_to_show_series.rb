42bd21693a288d0e515c2bf6e65fa5646029f19f
Tony Miller <mcfiredrill@gmail.com>
2024-01-15 14:52:12 -0800

    WIP

--- script/add_podcast_to_show_series.rb
+++ script/add_podcast_to_show_series.rb
@@ -3,13 +3,22 @@ require 'csv'
 csv_path = "/tmp/missing_podcasts.csv"
 
 CSV.foreach(csv_path, headers: true) do |row|
-  # show_title = row[:show_series]
   slug = row[:show_series_slug]
-  show_series = ShowSeries.friendly.find slug
-  track = Track.find_by(title: row[:title])
-  if show_series
-    episode = show_series.scheduled_shows.new title: row[:title], start_at: row[:created_at], end_at: row[:created_at], status: "archive_published"
-    episode.save!
-    track.update scheduled_show: episode
+  if slug
+    show_series = ShowSeries.friendly.find slug
+    track = Track.find_by(title: row[:audio_file_name])
+    if show_series
+      episode = show_series.scheduled_shows.new title: row[:title], start_at: row[:created_at], end_at: row[:created_at], status: "archive_published"
+      episode.save!
+      track.update scheduled_show: episode
+    end
+  else
+    title = row[:show_series]
+    show_series = ShowSeries.find_by title: title
+    if show_series
+    else
+      # create it
+      # ShowSeries.create user: ???
+    end
   end
 end
