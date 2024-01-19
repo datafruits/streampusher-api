5cf38b1f4b4f1a788c2a6c1e2e6adc8af218533f
Tony Miller <mcfiredrill@gmail.com>
2024-01-18 18:01:13 -0800

    fix title

--- script/add_podcast_to_show_series.rb
+++ script/add_podcast_to_show_series.rb
@@ -8,7 +8,9 @@ CSV.foreach(csv_path, headers: true) do |row|
     show_series = ShowSeries.friendly.find slug
     track = Track.find_by(title: row[:audio_file_name])
     if show_series
-      episode = show_series.scheduled_shows.new title: row[:title], start_at: row[:created_at], end_at: row[:created_at], status: "archive_published"
+      title = row[:title]
+      # strip date from show if it exists, somehow
+      episode = show_series.scheduled_shows.new title: title, start_at: row[:created_at], end_at: row[:created_at], status: "archive_published"
       episode.save!
       track.update scheduled_show: episode
     end
