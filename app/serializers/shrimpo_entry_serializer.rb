1201adb1321756011a03ed0086ecdac39d428358
Tony Miller <mcfiredrill@gmail.com>
2024-02-04 10:41:31 -0800

    add images to serializer

--- app/serializers/shrimpo_entry_serializer.rb
+++ app/serializers/shrimpo_entry_serializer.rb
@@ -1,10 +1,15 @@
 class ShrimpoEntrySerializer < ActiveModel::Serializer
-  attributes :title, :audio_file_url, :username
+  attributes :title, :audio_file_url, :username, :user_avatar
+  belongs_to :shrimpo
 
   def audio_file_url
   end
 
   def username
     object.user.username
+  end
+
+  def user_avatar
+    CGI.unescape(object.user.image.url(:thumb))
   end
 end
