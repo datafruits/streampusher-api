31c5b39e155a2948c22d6eaff3fff721f679a134
Tony Miller <mcfiredrill@gmail.com>
2024-03-22 23:27:03 -0700

    fix votes_controller

--- app/controllers/api/shrimpos/shrimpo_entries/votes_controller.rb
+++ app/controllers/api/shrimpos/shrimpo_entries/votes_controller.rb
@@ -4,10 +4,10 @@ class Api::Shrimpos::ShrimpoEntries::VotesController < ApplicationController
     vote = ShrimpoVote.find_or_initialize_by shrimpo_entry_id: shrimpo_entry.id, user_id: current_user.id
     vote.attributes = shrimpo_vote_params
     if vote.save
-      ActiveSupport::Notifications.instrument 'shrimpo.vote.created', title: shrimpo_entry.title, username: entry.user.username, score: vote.score
+      ActiveSupport::Notifications.instrument 'shrimpo.vote.created', title: shrimpo_entry.title, username: shrimpo_entry.user.username, score: vote.score
       render json: vote
     else
-      ActiveSupport::Notifications.instrument 'shrimpo.vote.create.error', title: shrimpo_entry.title, username: entry.user.username, score: vote.score, errors: vote.errors, params: params
+      ActiveSupport::Notifications.instrument 'shrimpo.vote.create.error', title: shrimpo_entry.title, username: shrimpo_entry.user.username, score: vote.score, errors: vote.errors, params: params
       render json: { errors: vote.errors }, status: 422
     end
   end
