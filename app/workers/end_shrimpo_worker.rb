636da92ff4ff9ffe28e543261523b3b9d03202ce
Tony Miller <mcfiredrill@gmail.com>
2024-03-29 04:43:44 -0700

    exit end_shrimpo_worker if not in running status

--- app/workers/end_shrimpo_worker.rb
+++ app/workers/end_shrimpo_worker.rb
@@ -3,6 +3,7 @@ class EndShrimpoWorker < ActiveJob::Base
 
   def perform shrimpo_id
     shrimpo = Shrimpo.find shrimpo_id
+    return unless shrimpo.running?
     shrimpo.voting!
     Notification.create! notification_type: :shrimpo_voting_started, source: shrimpo, send_to_chat: true, send_to_user: false
   end
