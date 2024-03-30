d6b2361a88f33785f8d4f8b4f9746f3e51c9de61
Tony Miller <mcfiredrill@gmail.com>
2024-03-30 12:14:37 -0700

    wip lost podcasts script

--- script/add_podcast_to_show_series.rb
+++ script/add_podcast_to_show_series.rb
@@ -1,27 +1,64 @@
+# TODO
 require 'csv'
 
 csv_path = "/tmp/missing_podcasts.csv"
 
+# should datafruits5, dxdf etc be under guest fruits?
+# or its own show series?
+show_series_user_mapping = {
+  "club trash" => "alptrack",
+  "case of the mondays" => "frail",
+  "chex mix" => "jallen250",
+  "dj supermarket" => "",
+  "dxdf" => "",
+  "essential fruits" => "mcfiredrill",
+  "good morning datafruits" => "oven",
+  "happy hybrids" => "tim",
+  "city hunter" => "",
+  "mmainframe" => "hpc",
+  "puro fantasia radio hrs" => "",
+  "the curios show" => "skotvoid",
+  "salinger" => "salinger",
+  "solar system" => "",
+  "mint jams" => "slugga",
+  "flat402" => "hyuga daichi",
+}
+
+# if there's a slug, find the show series and add a new episode
+# otherwise create the show series if there's a title under show_series col
+# if neither exist, add to guest fruits
 CSV.foreach(csv_path, headers: true) do |row|
   slug = row[:show_series_slug]
+  title = row[:show_series]
+  track = Track.find_by(title: row[:audio_file_name])
+  title = row[:title]
   if slug
     show_series = ShowSeries.friendly.find slug
-    track = Track.find_by(title: row[:audio_file_name])
     if show_series
-      title = row[:title]
       # strip date from show if it exists, somehow
       episode = show_series.scheduled_shows.new title: title, start_at: row[:created_at], end_at: row[:created_at], status: "archive_published"
       episode.save!
       track.update scheduled_show: episode
       episode.labels << track.labels
+    else
+      puts "couldn't find show series with slug: #{slug}"
     end
-  else
-    title = row[:show_series]
+  elsif title
     show_series = ShowSeries.find_by title: title
     if show_series
     else
       # create it
+      #
+      # find user by show_series_user_mapping
       # ShowSeries.create user: ???
     end
+  else
+    # add to guest fruits
+    guest_fruits = ShowSeries.find_by title: "Guest Fruits"
+    # TODO user??
+    episode = guest_fruits.episodes.new title: title, start_at: row[:created_at], end_at: row[:created_at], status: "archive_published"
+    episode.save!
+    track.update scheduled_show: episode
+    episode.labels << track.labels
   end
 end
