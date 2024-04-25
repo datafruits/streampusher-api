a67ca817dd66219edee5067f3f3ac7852867e9cd
Tony Miller <mcfiredrill@gmail.com>
2024-04-25 11:00:34 -0700

    fix script again

--- script/fix_guest_fruits_episodes.rb
+++ script/fix_guest_fruits_episodes.rb
@@ -11,7 +11,7 @@ CSV.foreach(csv_path, headers: true) do |row|
     slug = row["show_slug"]
     episode = ScheduledShow.friendly.find slug
     show_series = ShowSeries.friendly.find show_series_slug
-    episode.update! show_series_id: show_series.id
+    episode.update! show_series_id: show_series.id, status: "archive_published"
   else
     user = User.where("username ilike ?", "%#{username.strip}%").first
     if !user
@@ -22,8 +22,9 @@ CSV.foreach(csv_path, headers: true) do |row|
       user = datafruits.users.create! username: username.strip, password: rand1, password_confirmation: rand1, email: "test#{rand2}@datafruits.fm"
     end
     show = ScheduledShow.friendly.find(row["show_slug"])
+    show.update! status: "archive_published"
     if show.dj.id != user.id
-      show.update! dj: user, status: "archive_published"
+      show.update! dj: user
     end
   end
 end
