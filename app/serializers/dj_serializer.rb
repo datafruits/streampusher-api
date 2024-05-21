5041d7895a57b59bda1c93bdba3dfa2def6fa06e
Tony Miller <mcfiredrill@gmail.com>
2024-05-21 14:25:51 -0700

    remove-email-from-dj-serializer

--- app/serializers/dj_serializer.rb
+++ app/serializers/dj_serializer.rb
@@ -1,5 +1,5 @@
 class DjSerializer < ActiveModel::Serializer
-  attributes :id, :username, :image_url, :bio, :image, :style, :email,
+  attributes :id, :username, :image_url, :bio, :image, :style,
     :image_thumb_url, :image_medium_url, :profile_publish, :pronouns, :role, :fruits_affinity,
     :homepage, :fruit_ticket_balance, :created_at, :last_sign_in_at,
     :level, :experience_points, :xp_needed_for_next_level,
