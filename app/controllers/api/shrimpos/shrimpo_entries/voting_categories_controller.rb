04def38d1b9caf7f17e77c0969e011624781046e
Tony Miller <mcfiredrill@gmail.com>
2024-09-10 16:52:50 -0700

    update shrimpo vote unique index

--- app/controllers/api/shrimpos/shrimpo_entries/voting_categories_controller.rb
+++ app/controllers/api/shrimpos/shrimpo_entries/voting_categories_controller.rb
@@ -3,12 +3,13 @@ class Api::Shrimpos::ShrimpoEntries::VotingCategoriesController < ApplicationCon
     authorize! :vote, :shrimpo
     shrimpo_entry = ShrimpoEntry.friendly.find params["shrimpo_entry_id"]
     shrimpo = shrimpo_entry.shrimpo
-    params[:data].each do |voting_category|
-      category = shrimpo.shrimpo_voting_categories.find_by name: voting_category["attributes"]["category_name"]
-      binding.pry
-      vote = ShrimpoVote.find_or_initialize_by shrimpo_entry_id: shrimpo_entry.id, user_id: current_user.id, shrimpo_voting_category: category
-      vote.score = voting_category["attributes"]["category_name"]
-      vote.save!
+    ActiveRecord::Base.transaction do
+      params[:data].each do |voting_category|
+        category = shrimpo.shrimpo_voting_categories.find_by name: voting_category["attributes"]["category_name"]
+        vote = ShrimpoVote.find_or_initialize_by shrimpo_entry_id: shrimpo_entry.id, user_id: current_user.id, shrimpo_voting_category: category
+        vote.score = voting_category["attributes"]["score"]
+        vote.save!
+      end
     end
     render head: :ok
   end
