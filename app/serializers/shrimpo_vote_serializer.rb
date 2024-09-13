1a728e111c4f03c343b5ea2744d364854af84f50
Tony Miller <mcfiredrill@gmail.com>
2024-09-13 11:50:59 -0700

    shrimpo vote emoji in serializer

--- app/serializers/shrimpo_vote_serializer.rb
+++ app/serializers/shrimpo_vote_serializer.rb
@@ -1,10 +1,14 @@
 class ShrimpoVoteSerializer < ActiveModel::Serializer
-  attributes :score, :voting_category_name
+  attributes :score, :voting_category_name, :voting_category_emoji
   belongs_to :user
   belongs_to :shrimpo_entry
   belongs_to :shrimpo_voting_category
 
   def voting_category_name
     object.shrimpo_voting_category.name
+  end
+
+  def voting_category_emoji
+    object.shrimpo_voting_category.emoji
   end
 end
