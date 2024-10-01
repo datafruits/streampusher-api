d2ae82786164d8fd5f2d7dc082f302fe06aba58d
Tony Miller <mcfiredrill@gmail.com>
2024-09-30 18:50:44 -0700

    fix end shrimpo worker

--- app/workers/end_shrimpo_worker.rb
+++ app/workers/end_shrimpo_worker.rb
@@ -5,6 +5,6 @@ class EndShrimpoWorker < ActiveJob::Base
     shrimpo = Shrimpo.find shrimpo_id
     return unless shrimpo.running?
     shrimpo.voting!
-    Notification.create! notification_type: :shrimpo_voting_started, source: shrimpo, send_to_chat: true, send_to_user: false
+    Notification.create! notification_type: :shrimpo_voting_started, source: shrimpo, send_to_chat: true, send_to_user: false, user: shrimpo.user
   end
 end
