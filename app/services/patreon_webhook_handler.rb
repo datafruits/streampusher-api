6414dfc9905a61f513c512a6357f2af042dab1f3
Tony Miller <mcfiredrill@gmail.com>
2025-08-13 20:27:40 -0700

    only send patreon notification if they joined a non-free tier

--- app/services/patreon_webhook_handler.rb
+++ app/services/patreon_webhook_handler.rb
@@ -3,6 +3,8 @@ class PatreonWebhookHandler
     pledge_amount_cents = data["attributes"]["pledge_amount_cents"]
     name = data["attributes"]["full_name"].split(" ").first
     pledge = PatreonPledge.create! json_blob: data, name: name, pledge_amount_cents: pledge_amount_cents
-    Notification.create! notification_type: "patreon_sub", send_to_chat: true, send_to_user: false, source: pledge, user_id: 1
+    if pledge.tier_name.present?
+      Notification.create! notification_type: "patreon_sub", send_to_chat: true, send_to_user: false, source: pledge, user_id: 1
+    end
   end
 end
