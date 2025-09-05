d305e9a96813040673f01caa9f0e1992c3595984
Tony Miller <mcfiredrill@gmail.com>
2025-09-05 09:39:00 -0700

    don't call canonical metadata sync in scheduled_show

--- app/models/scheduled_show.rb
+++ app/models/scheduled_show.rb
@@ -127,8 +127,6 @@ class ScheduledShow < ActiveRecord::Base
       # this key is for compatibility ???? or what
       current_show = { title: self.title, user: self.dj.username, scheduled_show: self.id }
       StreamPusher.redis.hset "#{radio}:current_show", current_show
-      # why not just user CanonicalMetadataSync **only** here ?
-      CanonicalMetadataSync.perform radio.id, self.title
     else
       puts "tried to queue #{self.inspect}'s playlist, but playlist empty in redis!"
     end
