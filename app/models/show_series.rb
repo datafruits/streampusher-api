ab42b85f87937eca3971feae0f82003222a6804b
Tony Miller <mcfiredrill@gmail.com>
2025-09-25 13:18:59 -0700

    fix longstanding timezone bug in saving episodes

--- app/models/show_series.rb
+++ app/models/show_series.rb
@@ -99,14 +99,17 @@ class ShowSeries < ApplicationRecord
 
   def save_episodes
     if recurring?
-      start_day = DateTime.new recurrences.first.year,
-                               recurrences.first.month,
-                               recurrences.first.day,
-                               self.start_time.in_time_zone(self.time_zone).hour,
-                               self.start_time.in_time_zone(self.time_zone).min,
-                               0,
-                               ActiveSupport::TimeZone[self.time_zone].formatted_offset
+      start_day = Time.use_zone(self.time_zone) do
+        Time.zone.local(recurrences.first.year,
+                        recurrences.first.month,
+                        recurrences.first.day,
+                        self.start_time.in_time_zone(self.time_zone).hour,
+                        self.start_time.in_time_zone(self.time_zone).min,
+                        0
+                       )
 
+      end
+      start_day_dt = start_day.to_datetime
       recurrences.each do |r|
         scheduled_show = self.episodes.new
         scheduled_show.radio = self.radio
@@ -116,18 +119,25 @@ class ShowSeries < ApplicationRecord
         #
         # TODO use a reference instead of copying a million new images
         # scheduled_show.image = self.image if self.image.present?
-        new_start_at = DateTime.new(
-          r.year,
-          r.month,
-          r.day,
-          self.start_time.in_time_zone(self.time_zone).hour,
-          self.start_time.in_time_zone(self.time_zone).min,
-          0,
-          ActiveSupport::TimeZone[self.time_zone].formatted_offset)
-        if new_start_at > Time.now
-          difference_in_days = (new_start_at - start_day).to_i
+        #
+        # take into account the offset of this recurrence (could be DST or not)
+
+        new_start_at = Time.use_zone(self.time_zone) do
+          Time.zone.local(
+            r.year,
+            r.month,
+            r.day,
+            self.start_time.in_time_zone(self.time_zone).hour,
+            self.start_time.in_time_zone(self.time_zone).min,
+            0
+          )
+        end
+
+        new_start_at_dt = new_start_at.to_datetime
+        if new_start_at_dt > Time.now
+          difference_in_days = (new_start_at_dt - start_day_dt).to_i
           # have to calculate time with .advance to preserve correct hour across time zone boundry
-          start_at = start_day.in_time_zone(self.time_zone).advance(days: difference_in_days)
+          start_at = start_day_dt.in_time_zone(self.time_zone).advance(days: difference_in_days)
 
           scheduled_show.start_at = start_at
           scheduled_show.end_at = start_at + (((self.end_time - self.start_time).seconds / 60) / 60).round.hours
