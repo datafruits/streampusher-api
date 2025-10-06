2f97f2f5df9ed6dc0088d7308c8ba947cd90fca7
Tony Miller <mcfiredrill@gmail.com>
2025-10-05 18:53:19 -0700

    always assign track to scheduled show even if track is already processed

--- app/services/process_recording.rb
+++ app/services/process_recording.rb
@@ -28,24 +28,28 @@ class ProcessRecording
 
       # Check for existing track with same audio file for same scheduled show to prevent duplicates
       if scheduled_show.present?
+        track = nil
         existing_track = scheduled_show.tracks.find_by(audio_file_name: audio_file_name)
         if existing_track.present?
           Rails.logger.info "Track with audio file #{audio_file_name} already exists for scheduled show #{scheduled_show.id}"
-          recording.update processing_status: 'processed', track: existing_track
-          return existing_track
+          track = existing_track
+        else
+          username = basename.split("datafruits-").last.split("-").first
+          user = User.find_by(username: username)
+          track = radio.tracks.create! audio_file_name: audio_file_name, uploaded_by: user
         end
-      end
 
-      username = basename.split("datafruits-").last.split("-").first
-      user = User.find_by(username: username)
-      track = radio.tracks.create! audio_file_name: audio_file_name, uploaded_by: user
-
-      date = Chronic.parse basename.split("datafruits-").last.split("-")[1..3].join('-')
-
-      if scheduled_show.present?
         # grab scheduled show's info to use metadata and artwork
         track.update scheduled_show_id: scheduled_show.id, title: scheduled_show.formatted_episode_title
+
+      else
+        username = basename.split("datafruits-").last.split("-").first
+        user = User.find_by(username: username)
+        track = radio.tracks.create! audio_file_name: audio_file_name, uploaded_by: user
       end
+
+      recording.update processing_status: 'processed', track: track
+
       StreamingExpAwardWorker.set(wait: 15.minute).perform_later(track.id)
     rescue Exception => e
       recording.update processing_status: 'processing_failed'
