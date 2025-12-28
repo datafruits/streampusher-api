a902383a0f31d945e159496a69ada819cb39b442
Tony Miller <mcfiredrill@gmail.com>
2025-12-28 14:30:50 -0800

    show guest fruits episodes for user episodes

--- app/controllers/api/djs/episodes_controller.rb
+++ app/controllers/api/djs/episodes_controller.rb
@@ -3,16 +3,41 @@ class Api::Djs::EpisodesController < ApplicationController
     djs = @current_radio.djs
       .includes(show_series_hosts: [ :show_series ]).where(enabled: true)
     @dj = djs.find(params[:dj_id])
-    show_series = ShowSeries.joins("inner join show_series_hosts on show_series_hosts.user_id = #{@dj.id} and show_series_hosts.show_series_id = show_series.id")
+
+    hosted_series_ids = ShowSeries
+      .joins(:show_series_hosts)
+      .where(show_series_hosts: { user_id: @dj.id })
+      .pluck(:id)
+
+    # need to also check GuestFruits
+    guest_fruits_series_id = ShowSeries
+      .where(title: "GuestFruits")
+      .pick(:id)
+
+    guest_show_ids =
+      if guest_fruits_series_id
+        ScheduledShow
+          .joins(:show_series)
+          .where(
+            dj_id: @dj.id ,
+            show_series_id: guest_fruits_series_id
+          )
+            .pluck(:id)
+      else
+        []
+      end
+
       shows = @current_radio.scheduled_shows
         .includes([:tracks])
-        .where(show_series_id: show_series.pluck(:id))
+        .where(
+          "show_series_id IN (?) OR scheduled_shows.id IN (?)",
+          hosted_series_ids,
+          guest_show_ids
+        )
         .where(status: :archive_published)
         .order("start_at DESC")
     shows = shows.page(params[:page])
 
-    # meta = { page: params[:page], total_pages: shows.total_pages.to_i }
-    # render json: shows, meta: meta
     options = {}
     options[:meta] = { page: params[:page], total_pages: shows.total_pages.to_i }
     options[:include] = ['tracks']
