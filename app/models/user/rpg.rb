fe2076f3bae324077b7813a720d2aa6b49337859
Tony Miller <mcfiredrill@gmail.com>
2026-02-24 14:20:56 -0800

    add user xp progress to serializer

--- app/models/user/rpg.rb
+++ app/models/user/rpg.rb
@@ -5,14 +5,20 @@ module User::Rpg
     next_level - self.experience_points
   end
 
-  def next_level
-    if self.level === 0
-      return 32
-    elsif self.level === 1
+  def xp_required_for_level(level)
+    if level <= 0
+      0
+    elsif level == 1
+      32
+    elsif level == 2
       500
     else
-      return 500 * (self.level ** 2) - (500 * self.level)
+      500 * (level ** 2) - (500 * level)
     end
+  end
+
+  def next_level
+    return xp_required_for_level(self.level)
   end
 
   def should_level_up?
@@ -24,5 +30,9 @@ module User::Rpg
     self.save!
     Notification.create notification_type: "level_up", user: self, send_to_chat: true
     ActiveSupport::Notifications.instrument 'user.level_up', username: self.username, level: self.level
+  end
+
+  def xp_progress_percentage
+    (self.experience_points.to_f / self.next_level) * 100
   end
 end
