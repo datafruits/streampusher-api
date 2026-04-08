b1595cc6b9d5ccfddc9557cb3b9d04065dad798e
Tony Miller <mcfiredrill@gmail.com>
2026-04-08 09:48:44 -0700

    fix serving app

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ Bundler.require(*Rails.groups)
 module StreamPusher
   class Application < Rails::Application
     config.api_only = true
+    config.assets.paths << Rails.root.join('app', 'javascript')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
