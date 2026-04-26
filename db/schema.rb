ff8512fcaac7d76dcbab05e37502c23a6d64c8cb
Tony Miller <mcfiredrill@gmail.com>
2026-04-26 11:31:23 -0700

    save glorp lotteries to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
+ActiveRecord::Schema[7.0].define(version: 2026_04_26_181859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -138,6 +138,14 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
     t.datetime "updated_at", precision: nil, null: false
   end
 
+  create_table "glorp_lotteries", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "amount", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_glorp_lotteries_on_user_id"
+  end
+
   create_table "host_applications", id: :serial, force: :cascade do |t|
     t.integer "radio_id", null: false
     t.string "email", null: false
@@ -164,7 +172,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
 
   create_table "listens", id: :serial, force: :cascade do |t|
     t.integer "radio_id"
-    t.string "ip_address"
+    t.string "ip_address", limit: 255
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
     t.datetime "start_at", precision: nil
@@ -237,7 +245,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
     t.integer "radio_id", null: false
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
-    t.string "name"
+    t.string "name", limit: 255
     t.integer "interpolated_playlist_id"
     t.integer "interpolated_playlist_track_play_count"
     t.integer "interpolated_playlist_track_interval_count"
@@ -266,7 +274,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
     t.integer "playlist_id"
     t.string "image_file_name"
     t.string "image_content_type"
-    t.bigint "image_file_size"
+    t.integer "image_file_size"
     t.datetime "image_updated_at", precision: nil
     t.string "extra_tags"
     t.index ["playlist_id"], name: "index_podcasts_on_playlist_id"
@@ -285,21 +293,21 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
   end
 
   create_table "radios", id: :serial, force: :cascade do |t|
-    t.string "icecast_container_id"
+    t.string "icecast_container_id", limit: 255
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
-    t.string "name", default: "", null: false
-    t.string "liquidsoap_container_id"
+    t.string "name", limit: 255, default: "", null: false
+    t.string "liquidsoap_container_id", limit: 255
     t.integer "default_playlist_id"
     t.boolean "enabled", default: true, null: false
     t.boolean "vj_enabled", default: false, null: false
     t.boolean "podcasts_enabled", default: false, null: false
     t.boolean "stats_enabled", default: false, null: false
-    t.boolean "social_identities_enabled", default: false, null: false
     t.string "tunein_partner_id"
     t.string "tunein_partner_key"
     t.string "tunein_station_id"
     t.boolean "tunein_metadata_updates_enabled", default: false, null: false
+    t.boolean "social_identities_enabled", default: false, null: false
     t.string "container_name", null: false
     t.boolean "schedule_monitor_enabled", default: false, null: false
     t.string "show_share_url"
@@ -362,7 +370,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
     t.text "description"
     t.string "image_file_name"
     t.string "image_content_type"
-    t.bigint "image_file_size"
+    t.integer "image_file_size"
     t.datetime "image_updated_at", precision: nil
     t.integer "recurring_interval", default: 0, null: false
     t.boolean "recurrence", default: false, null: false
@@ -433,7 +441,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
   end
 
   create_table "shows", id: :serial, force: :cascade do |t|
-    t.string "title", default: "", null: false
+    t.string "title", limit: 255, default: "", null: false
     t.integer "dj_id", null: false
     t.integer "radio_id", null: false
     t.text "description", default: "", null: false
@@ -443,7 +451,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
     t.string "color"
     t.string "image_file_name"
     t.string "image_content_type"
-    t.bigint "image_file_size"
+    t.integer "image_file_size"
     t.datetime "image_updated_at", precision: nil
   end
 
@@ -528,6 +536,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
     t.string "provider", default: "", null: false
     t.integer "user_id", null: false
     t.string "token"
+    t.string "string"
     t.string "token_secret"
     t.string "name", default: "", null: false
     t.datetime "created_at", precision: nil, null: false
@@ -556,7 +565,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
   end
 
   create_table "tracks", id: :serial, force: :cascade do |t|
-    t.string "audio_file_name"
+    t.string "audio_file_name", limit: 255
     t.integer "radio_id"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
@@ -566,19 +575,19 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
     t.string "album"
     t.integer "year"
     t.integer "track"
-    t.integer "length"
     t.integer "filesize", default: 0, null: false
     t.integer "tag_processing_status", default: 0, null: false
+    t.integer "length"
     t.string "artwork_file_name"
     t.string "artwork_content_type"
-    t.bigint "artwork_file_size"
+    t.integer "artwork_file_size"
     t.datetime "artwork_updated_at", precision: nil
     t.integer "mixcloud_upload_status", default: 0, null: false
     t.string "mixcloud_key"
-    t.integer "soundcloud_upload_status", default: 0, null: false
-    t.string "soundcloud_key"
     t.integer "uploaded_by_id"
     t.integer "scheduled_show_id"
+    t.integer "soundcloud_upload_status", default: 0, null: false
+    t.string "soundcloud_key"
     t.string "youtube_link"
     t.index ["radio_id"], name: "index_tracks_on_radio_id"
     t.index ["scheduled_show_id"], name: "index_tracks_on_scheduled_show_id"
@@ -622,6 +631,21 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
     t.index ["user_id"], name: "index_user_accessories_on_user_id"
   end
 
+  create_table "user_emojis", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_emojis_on_user_id"
+  end
+
+  create_table "user_live_bars", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_live_bars_on_user_id"
+  end
+
   create_table "user_radios", id: :serial, force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "radio_id", null: false
@@ -633,20 +657,20 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
+    t.string "email", limit: 255, default: "", null: false
+    t.string "encrypted_password", limit: 255, default: "", null: false
+    t.string "reset_password_token", limit: 255
     t.datetime "reset_password_sent_at", precision: nil
     t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at", precision: nil
     t.datetime "last_sign_in_at", precision: nil
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
+    t.string "current_sign_in_ip", limit: 255
+    t.string "last_sign_in_ip", limit: 255
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
-    t.string "role"
-    t.string "username", default: "", null: false
+    t.string "role", limit: 255
+    t.string "username", limit: 255, default: "", null: false
     t.string "time_zone"
     t.string "display_name", default: "", null: false
     t.datetime "deleted_at", precision: nil
@@ -655,7 +679,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
     t.text "bio"
     t.string "image_file_name"
     t.string "image_content_type"
-    t.bigint "image_file_size"
+    t.integer "image_file_size"
     t.datetime "image_updated_at", precision: nil
     t.boolean "profile_publish", default: false, null: false
     t.integer "style", default: 0, null: false
@@ -666,6 +690,17 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_20_032802) do
     t.integer "level", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "versions", id: :serial, force: :cascade do |t|
+    t.string "item_type", null: false
+    t.integer "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object"
+    t.datetime "created_at", precision: nil
+    t.text "object_changes"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   create_table "wiki_page_edits", force: :cascade do |t|
