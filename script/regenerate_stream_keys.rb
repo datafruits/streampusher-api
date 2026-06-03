fea6331c9a6c7f75566d3b442030f4689b29a88b
Tony Miller <mcfiredrill@gmail.com>
2026-06-02 22:10:44 -0700

    rescue in regenerate_stream_keys

--- script/regenerate_stream_keys.rb
+++ script/regenerate_stream_keys.rb
@@ -2,4 +2,6 @@ User.find_each do |user|
   user.stream_key = nil
   user.save!
   puts "Regenerated stream key for user #{user.id} (#{user.username})"
+rescue => e
+  puts "ERROR regenerating stream key for user #{user.id} (#{user.username}): #{e.message}"
 end
