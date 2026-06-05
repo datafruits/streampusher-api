335694cbc148f977fef4a46b67f8527083a809a0
Tony Miller <mcfiredrill@gmail.com>
2026-06-04 17:20:17 -0700

    use published/unpublished param for episodes_controller

--- app/controllers/api/show_series/episodes_controller.rb
+++ app/controllers/api/show_series/episodes_controller.rb
@@ -1,13 +1,10 @@
 class Api::ShowSeries::EpisodesController < ApplicationController
   def index
     episodes = ::ShowSeries.friendly.find(params[:show_series_id]).episodes
-    # FIXME change the query param naming
-    # past episodes, could be published or unpublished
     if params[:status] === "archive_published"
-      episodes = episodes.where("start_at < ?", Time.now).order("start_at DESC")
-    # future episodes start at ASC shouldn't be published
+      episodes = episodes.archive_published.order("start_at DESC")
     elsif params[:status] === "archive_unpublished"
-      episodes = episodes.where("start_at > ?", Time.now).order("start_at ASC")
+      episodes = episodes.archive_unpublished.order("start_at ASC")
     end
     episodes = episodes.page(params[:page])
     options = {}
